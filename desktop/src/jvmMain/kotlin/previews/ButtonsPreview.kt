7de01326f824624d5986c98828e5c1f10fa81947
andresmr <andres@dhis2.org>
2023-07-04 15:17:07 +0200

    Adding desktop previews

---
+++ desktop/src/jvmMain/kotlin/previews/ButtonsPreview.kt
@@ -0,0 +1,18 @@
+package previews
+
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.runtime.Composable
+import org.hisp.dhis.common.components.Dhis2ButtonPreview
+import org.hisp.dhis.common.components.Dhis2TextButtonPreview
+
+@Preview
+@Composable
+fun TextButtonPreview() {
+    Dhis2TextButtonPreview()
+}
+
+@Preview
+@Composable
+fun ButtonPReview() {
+    Dhis2ButtonPreview()
+}
