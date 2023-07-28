493312790e60fb686a3d385a2325f9ec17aea312
Andrés Miguel Rubio <andres@dhis2.org>
2023-07-28 16:52:35 +0200

    update compose version to 1.4.3 (#5)
    * update compose version to 1.4.3
    
    * Update github-ci.yml
    
    * Update github-ci.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,6 @@
 group = "org.hisp.dhis"
 version = "1.0-SNAPSHOT"
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-    }
-}
-
 plugins {
     kotlin("multiplatform") apply false
     id("com.android.application") apply false
