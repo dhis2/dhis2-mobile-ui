c12ace111b72b7725cfe9144e965bcfb56245d3f
andresmr <andres@dhis2.org>
2023-07-29 17:51:55 +0200

    Adding IconButton

--- common/src/commonMain/kotlin/org/hisp/dhis/common/designsystem/component/Button.kt
+++ common/src/commonMain/kotlin/org/hisp/dhis/common/designsystem/component/Button.kt
@@ -1,18 +1,55 @@
 package org.hisp.dhis.common.designsystem.component
 
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import org.hisp.dhis.common.designsystem.icon.Icons
+import org.hisp.dhis.common.designsystem.theme.Dimen
 
 @Composable
 fun SquareIconButton(
+    enabled: Boolean = true,
     icon: @Composable (() -> Unit),
     onClick: () -> Unit
 ) {
-    Button(
+    ElevatedButton(
+        onClick = onClick,
         modifier = Modifier,
+        enabled = enabled,
+        shape = RoundedCornerShape(10.dp),
+        contentPadding = PaddingValues(Dimen.spacing4)
+    ) {
+        icon()
+    }
+}
+
+@Composable
+internal fun SquareIconButtonPreview(enabled: Boolean = true) {
+    SquareIconButton(
+        enabled = enabled,
+        icon = {
+            Icon(
+                imageVector = Icons.FileDownload,
+                contentDescription = ""
+            )
+        } // TODO("Do we need content description?")
+    ) { }
+}
+
+@Composable
+fun IconButton(
+    enabled: Boolean = true,
+    icon: @Composable (() -> Unit),
+    onClick: () -> Unit
+) {
+    IconButton(
+        modifier = Modifier,
+        enabled = enabled,
         onClick = onClick
     ) {
         icon()
@@ -20,8 +57,9 @@ fun SquareIconButton(
 }
 
 @Composable
-internal fun SquareIconButtonPreview() {
-    SquareIconButton(
+internal fun IconButtonPreview(enabled: Boolean = true) {
+    IconButton(
+        enabled = enabled,
         icon = {
             Icon(
                 imageVector = Icons.FileDownload,
--- common/src/commonMain/kotlin/org/hisp/dhis/common/screens/ButtonScreen.kt
+++ common/src/commonMain/kotlin/org/hisp/dhis/common/screens/ButtonScreen.kt
@@ -1,12 +1,17 @@
 package org.hisp.dhis.common.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import org.hisp.dhis.common.designsystem.component.Dhis2ButtonPreview
 import org.hisp.dhis.common.designsystem.component.Dhis2TextButtonPreview
+import org.hisp.dhis.common.designsystem.component.IconButtonPreview
 import org.hisp.dhis.common.designsystem.component.SquareIconButtonPreview
 
 @Composable
@@ -14,6 +19,24 @@ fun ButtonScreen() {
     Column(modifier = Modifier.padding(10.dp)) {
         Dhis2TextButtonPreview()
         Dhis2ButtonPreview()
-        SquareIconButtonPreview()
+        Column(modifier = Modifier.padding(10.dp)) {
+            Text(text = "SquareIconButton", fontWeight = FontWeight.Bold)
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(10.dp)
+            ) {
+                SquareIconButtonPreview()
+                SquareIconButtonPreview(false)
+            }
+        }
+
+        Column(modifier = Modifier.padding(10.dp)) {
+            Text(text = "IconButton", fontWeight = FontWeight.Bold)
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(10.dp)
+            ) {
+                IconButtonPreview()
+                IconButtonPreview(false)
+            }
+        }
     }
 }
