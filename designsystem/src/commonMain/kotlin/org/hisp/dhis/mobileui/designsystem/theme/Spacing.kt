4ccae6a8e75535ad2c8c7e347ebd380db5ef7273
andresmr <andres@dhis2.org>
2023-08-02 11:31:41 +0200

    Adding Spacing dimensions

--- designsystem/src/commonMain/kotlin/org/hisp/dhis/mobileui/designsystem/theme/Spacing.kt
+++ designsystem/src/commonMain/kotlin/org/hisp/dhis/mobileui/designsystem/theme/Spacing.kt
@@ -4,6 +4,21 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 object Spacing {
+    val spacing2: Dp = 2.dp
     val spacing4: Dp = 4.dp
     val spacing8: Dp = 8.dp
+    val spacing16: Dp = 16.dp
+    val spacing24: Dp = 24.dp
+    val spacing32: Dp = 32.dp
+    val spacing40: Dp = 40.dp
+    val spacing48: Dp = 48.dp
+    val spacing56: Dp = 56.dp
+    val spacing64: Dp = 64.dp
+    val spacing72: Dp = 72.dp
+    val spacing80: Dp = 80.dp
+    val spacing88: Dp = 88.dp
+    val spacing96: Dp = 96.dp
+    val spacing104: Dp = 104.dp
+    val spacing112: Dp = 112.dp
+    val spacing120: Dp = 120.dp
 }
