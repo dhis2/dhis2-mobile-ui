2f5b6ed4cfbe59a229b4ec404d61d93fb44cc805
= <david.aparicio@dhis2.org>
2023-08-04 14:22:04 +0200

    add: [ANDROAPP-5428] Ripple theme added

--- designsystem/src/commonMain/kotlin/org/hisp/dhis/mobileui/designsystem/component/RadioButton.kt
+++ designsystem/src/commonMain/kotlin/org/hisp/dhis/mobileui/designsystem/component/RadioButton.kt
@@ -6,13 +6,16 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.ripple.LocalRippleTheme
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.RadioButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import org.hisp.dhis.mobileui.designsystem.theme.CustomDHISRippleTheme
 import org.hisp.dhis.mobileui.designsystem.theme.DHISTypography
 import org.hisp.dhis.mobileui.designsystem.theme.Outline
 import org.hisp.dhis.mobileui.designsystem.theme.Spacing
@@ -25,35 +28,21 @@ fun RadioButton(
     enabled: Boolean,
     textInput: String?,
     onClick: () -> Unit) {
-        if (textInput.isNullOrEmpty()) {
-            RadioButton(
-                selected = selected,
-                onClick = onClick,
-                enabled = enabled,
-                modifier = Modifier
-                    .width(Spacing.Spacing40)
-                    .height(Spacing.Spacing40)
-                    .padding(start = Spacing.Spacing8, top = Spacing.Spacing8, end = Spacing.Spacing8, bottom = Spacing.Spacing8),
-                colors = RadioButtonDefaults.colors(
-                    selectedColor = SurfaceColor.Primary,
-                    unselectedColor = Outline.Dark,
-                    disabledSelectedColor = TextColor.OnDisabledSurface,
-                    disabledUnselectedColor = TextColor.OnDisabledSurface
-                )
-            )
-        } else {
-            Row(
-                modifier = Modifier.width(840.dp).height(Spacing.Spacing40),
-                horizontalArrangement = Arrangement.spacedBy(0.dp, Alignment.Start),
-                verticalAlignment = Alignment.Top,
-            )  {
+        CompositionLocalProvider(LocalRippleTheme provides CustomDHISRippleTheme) {
+            if (textInput.isNullOrEmpty()) {
                 RadioButton(
                     selected = selected,
                     onClick = onClick,
                     enabled = enabled,
                     modifier = Modifier
                         .width(Spacing.Spacing40)
-                        .height(Spacing.Spacing40),
+                        .height(Spacing.Spacing40)
+                        .padding(
+                            start = Spacing.Spacing8,
+                            top = Spacing.Spacing8,
+                            end = Spacing.Spacing8,
+                            bottom = Spacing.Spacing8
+                        ),
                     colors = RadioButtonDefaults.colors(
                         selectedColor = SurfaceColor.Primary,
                         unselectedColor = Outline.Dark,
@@ -61,27 +50,48 @@ fun RadioButton(
                         disabledUnselectedColor = TextColor.OnDisabledSurface
                     )
                 )
+            } else {
                 Row(
-                    modifier = Modifier
-                        .width(800.dp)
-                        .height(40.dp)
-                        .padding(top = Spacing.Spacing8, bottom = Spacing.Spacing8),
+                    modifier = Modifier.width(840.dp).height(Spacing.Spacing40),
                     horizontalArrangement = Arrangement.spacedBy(0.dp, Alignment.Start),
                     verticalAlignment = Alignment.Top,
                 ) {
-                    Text(
+                    RadioButton(
+                        selected = selected,
+                        onClick = onClick,
+                        enabled = enabled,
+                        modifier = Modifier
+                            .width(Spacing.Spacing40)
+                            .height(Spacing.Spacing40),
+                        colors = RadioButtonDefaults.colors(
+                            selectedColor = SurfaceColor.Primary,
+                            unselectedColor = Outline.Dark,
+                            disabledSelectedColor = TextColor.OnDisabledSurface,
+                            disabledUnselectedColor = TextColor.OnDisabledSurface
+                        )
+                    )
+                    Row(
                         modifier = Modifier
                             .width(800.dp)
-                            .height(Spacing.Spacing24)
-                            .clickable { onClick() },
-                        text = textInput,
-                        style = DHISTypography.bodyLarge,
-                        color = if (enabled) {
-                            TextColor.OnSurface
-                        } else {
-                            TextColor.OnDisabledSurface
-                        },
-                    )
+                            .height(40.dp)
+                            .padding(top = Spacing.Spacing8, bottom = Spacing.Spacing8),
+                        horizontalArrangement = Arrangement.spacedBy(0.dp, Alignment.Start),
+                        verticalAlignment = Alignment.Top,
+                    ) {
+                        Text(
+                            modifier = Modifier
+                                .width(800.dp)
+                                .height(Spacing.Spacing24)
+                                .clickable { onClick() },
+                            text = textInput,
+                            style = DHISTypography.bodyLarge,
+                            color = if (enabled) {
+                                TextColor.OnSurface
+                            } else {
+                                TextColor.OnDisabledSurface
+                            },
+                        )
+                    }
                 }
             }
         }
