c3321a7b132b1c12691e06b9d2cddeba9d1d693a
andresmr <andres@dhis2.org>
2023-08-07 11:49:12 +0200

    Add moko-resources library for common resources

--- designsystem/src/commonMain/kotlin/org/hisp/dhis/mobileui/designsystem/theme/Type.kt
+++ designsystem/src/commonMain/kotlin/org/hisp/dhis/mobileui/designsystem/theme/Type.kt
@@ -4,19 +4,21 @@ import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import dev.icerock.moko.resources.compose.fontFamilyResource
+import org.hisp.dhis.mobileui.designsystem.library.MR
 
-internal val DHISTypography = Typography(
+/* internal val DHISTypography = Typography(
     titleLarge = TextStyle(
         fontSize = 22.sp,
         lineHeight = 28.sp,
-//        fontFamily = FontFamily(Font(R.font.roboto)),
+        fontFamily = fontFamilyResource(MR.fonts.cormorant.italic),
         fontWeight = FontWeight.Normal,
         color = TextColor.OnSurface
     ),
     titleMedium = TextStyle(
         fontSize = 16.sp,
         lineHeight = 24.sp,
-//        fontFamily = FontFamily(Font(R.font.roboto)),
+//        fontFamily = FontFamily(MR..fonts.Roboto).asF,
         fontWeight = FontWeight.Medium,
         color = TextColor.OnSurface,
         letterSpacing = 0.15.sp
@@ -77,4 +79,4 @@ internal val DHISTypography = Typography(
         color = TextColor.OnSurface,
         letterSpacing = 0.4.sp
     )
-)
+)*/
