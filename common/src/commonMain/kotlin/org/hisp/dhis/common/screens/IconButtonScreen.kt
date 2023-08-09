cc11ae85c5800fddd29fc894977eb1848bbdd4ae
Xavier Molloy <xavi.molloy@gmail.com>
2023-08-09 16:22:32 +0200

    small corrections and refactoring

--- common/src/commonMain/kotlin/org/hisp/dhis/common/screens/IconButtonScreen.kt
+++ common/src/commonMain/kotlin/org/hisp/dhis/common/screens/IconButtonScreen.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import org.hisp.dhis.common.screens.previews.IconButtonPreview
+import org.hisp.dhis.common.screens.previews.SquareIconButtonPreview
 import org.hisp.dhis.mobileui.designsystem.component.IconButtonStyle
 
 @Composable
