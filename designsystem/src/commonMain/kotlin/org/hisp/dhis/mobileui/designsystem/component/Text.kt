b29efeff2795372edef34574ccf336fef921eddd
Xavier Molloy <xavi.molloy@gmail.com>
2023-08-10 15:05:24 +0200

    extract Text in button component to external class and code clean up

---
+++ designsystem/src/commonMain/kotlin/org/hisp/dhis/mobileui/designsystem/component/Text.kt
@@ -0,0 +1,32 @@
+package org.hisp.dhis.mobileui.designsystem.component
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import org.hisp.dhis.mobileui.designsystem.theme.Spacing
+
+/**
+ * DHIS2 Text with generic icon slot.
+ * @param text The text to display within.
+ * @param textColor The color of text to display within.
+ * @param icon The icon content is optional.
+ * Content will be centered and if there is an Icon
+ * required Button component spacing will be applied
+ */
+@Composable
+fun ButtonText(
+    text: String,
+    textColor: Color,
+    icon: @Composable
+    (() -> Unit)? = null
+) {
+    icon?.let {
+        it.invoke()
+        Spacer(Modifier.size(Spacing.Spacing8))
+    }
+    Text(text, color = textColor, textAlign = TextAlign.Center)
+}
