ee5bd47cf5f633832fcde9bc7eedb188b7906346
Xavier Molloy <xavi.molloy@gmail.com>
2023-08-17 12:57:45 +0200

    add clickable text and specific style

--- common/src/commonMain/kotlin/org/hisp/dhis/common/screens/SupportingTextScreen.kt
+++ common/src/commonMain/kotlin/org/hisp/dhis/common/screens/SupportingTextScreen.kt
@@ -1,16 +1,51 @@
 package org.hisp.dhis.common.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import org.hisp.dhis.common.screens.previews.SupportingTextPreview
 import org.hisp.dhis.mobileui.designsystem.component.SupportingTextState
+import org.hisp.dhis.mobileui.designsystem.theme.Spacing
 
 @Composable
 fun SupportingTextScreen() {
-    Column {
-        SupportingTextPreview("Supporting Text")
-        SupportingTextPreview("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas dolor lacus, aliquam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas dolor lacus, aliquam.Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+    Column(verticalArrangement = Arrangement.spacedBy(Spacing.Spacing10)) {
+        Spacer(Modifier.size(Spacing.Spacing24))
+        Text("Standard Supporting Text")
+        SupportingTextPreview("Supporting text")
+        Text("Standard Warning Supporting Text")
         SupportingTextPreview("Supporting Text", SupportingTextState.WARNING)
+        Text("Standard Error Supporting Text")
         SupportingTextPreview("Supporting Text", SupportingTextState.ERROR)
+        Text("Overflow Default Supporting Text")
+        SupportingTextPreview(
+            "Lorem ipsum dolor sit amet," +
+                " consectetur adipiscing elit. Maecenas dolor lacus," +
+                " aliquam. Lorem ipsum dolor sit amet," +
+                " consectetur adipiscing elit. Maecenas dolor lacus," +
+                " aliquam.Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        )
+        Text("Overflow Warning Supporting Text")
+        SupportingTextPreview(
+            "Lorem ipsum dolor sit amet," +
+                " consectetur adipiscing elit. Maecenas dolor lacus," +
+                " aliquam. Lorem ipsum dolor sit amet," +
+                " consectetur adipiscing elit. Maecenas dolor lacus," +
+                " aliquam.Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            SupportingTextState.WARNING
+        )
+        Text("Overflow Error Supporting Text")
+        SupportingTextPreview(
+            "Lorem ipsum dolor sit amet," +
+                " consectetur adipiscing elit. Maecenas dolor lacus," +
+                " aliquam. Lorem ipsum dolor sit amet," +
+                " consectetur adipiscing elit. Maecenas dolor lacus," +
+                " aliquam.Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            SupportingTextState.ERROR
+        )
     }
 }
--- designsystem/src/commonMain/kotlin/org/hisp/dhis/mobile/ui/designsystem/component/SupportingText.kt
+++ designsystem/src/commonMain/kotlin/org/hisp/dhis/mobile/ui/designsystem/component/SupportingText.kt
@@ -1,8 +1,8 @@
 package org.hisp.dhis.mobileui.designsystem.component
 
 import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.clickable
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -11,10 +11,17 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.ParagraphStyle
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextLayoutResult
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.sp
+import org.hisp.dhis.mobileui.designsystem.theme.Spacing
 import org.hisp.dhis.mobileui.designsystem.theme.SurfaceColor
 import org.hisp.dhis.mobileui.designsystem.theme.TextColor
-
 
 @Composable
 fun SupportingText(text: String, state: SupportingTextState = SupportingTextState.DEFAULT) {
@@ -22,6 +29,22 @@ fun SupportingText(text: String, state: SupportingTextState = SupportingTextStat
     val textLayoutResultState = remember { mutableStateOf<TextLayoutResult?>(null) }
     var isClickable by remember { mutableStateOf(false) }
     var finalText by remember { mutableStateOf(text) }
+    var annotatedText by remember {
+        mutableStateOf(
+            buildAnnotatedString {
+                withStyle(
+                    style = SpanStyle(
+                        color = state.color,
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight.Normal,
+                        letterSpacing = 0.25.sp
+                    )
+                ) {
+                    append(text)
+                }
+            }
+        )
+    }
 
     val textLayoutResult = textLayoutResultState.value
     LaunchedEffect(textLayoutResult) {
@@ -29,31 +52,85 @@ fun SupportingText(text: String, state: SupportingTextState = SupportingTextStat
 
         when {
             isExpanded -> {
-                finalText = "$text Show Less"
+                annotatedText = buildAnnotatedString {
+                    finalText = "$text "
+                    withStyle(style = ParagraphStyle(lineHeight = 20.sp)) {
+                        withStyle(
+                            style = SpanStyle(
+                                color = state.color,
+                                fontSize = 14.sp,
+                                fontWeight = FontWeight.Normal,
+                                letterSpacing = 0.25.sp
+                            )
+                        ) {
+                            append(finalText)
+                        }
+                        withStyle(
+                            style = SpanStyle(
+                                textDecoration = TextDecoration.Underline,
+                                fontSize = 14.sp,
+                                fontWeight = FontWeight(500),
+                                color = state.color,
+                                letterSpacing = 0.1.sp
+                            )
+                        ) {
+                            append("Show Less")
+                        }
+                    }
+                }
             }
             !isExpanded && textLayoutResult.hasVisualOverflow -> {
                 val lastCharIndex = textLayoutResult.getLineEnd(0)
-                val showMoreString = "... Show More"
+                val showMoreString = "Show More"
                 val adjustedText = text
                     .substring(startIndex = 0, endIndex = lastCharIndex)
-                    .dropLast(showMoreString.length)
+                    .dropLast(showMoreString.length + 5)
                     .dropLastWhile { it == ' ' || it == '.' }
 
-                finalText = "$adjustedText$showMoreString"
+                finalText = "$adjustedText...   "
+
+                annotatedText = buildAnnotatedString {
+                    withStyle(style = ParagraphStyle(lineHeight = 20.sp)) {
+                        withStyle(
+                            style = SpanStyle(
+                                color = state.color,
+                                fontSize = 14.sp,
+                                fontWeight = FontWeight.Normal,
+                                letterSpacing = 0.25.sp
+                            )
+                        ) {
+                            append(finalText)
+                        }
+                        withStyle(
+                            SpanStyle(
+                                textDecoration = TextDecoration.Underline,
+                                fontSize = 14.sp,
+                                fontWeight = FontWeight(500),
+                                color = state.color,
+                                letterSpacing = 0.1.sp
+                            )
+                        ) {
+                            append("Show more")
+                        }
+                    }
+                }
 
                 isClickable = true
             }
         }
     }
 
-    Text(
-        text = finalText,
-        color = state.color,
+    ClickableText(
+        text = annotatedText,
+
         maxLines = if (isExpanded) Int.MAX_VALUE else 1,
         onTextLayout = { textLayoutResultState.value = it },
-        modifier = Modifier
-            .clickable(enabled = isClickable) { isExpanded = !isExpanded }
-            .animateContentSize()
+        onClick = {
+            if (isClickable) {
+                isExpanded = !isExpanded
+            }
+        },
+        modifier = Modifier.animateContentSize().padding(start = Spacing.Spacing16, top = Spacing.Spacing4, end = Spacing.Spacing16)
     )
 }
 
