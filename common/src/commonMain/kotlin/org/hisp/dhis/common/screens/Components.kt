ab5e4a660bf5a356187e2b418528672664ad83f1
Pablo <ppajueloc@gmail.com>
2023-08-18 12:26:00 +0200

    ktlint

--- common/src/commonMain/kotlin/org/hisp/dhis/common/screens/Components.kt
+++ common/src/commonMain/kotlin/org/hisp/dhis/common/screens/Components.kt
@@ -1,6 +1,6 @@
 package org.hisp.dhis.common.screens
 
-enum class Components(val label:String) {
+enum class Components(val label: String) {
     ICON_BUTTON("Icon Button"),
     BUTTON("Button"),
     RADIO("Radio"),
