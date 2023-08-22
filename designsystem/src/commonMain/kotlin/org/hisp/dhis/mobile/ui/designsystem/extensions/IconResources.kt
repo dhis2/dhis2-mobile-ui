8542b169be81433a966f6d534f589a0e5ec55649
andresmr <andres@dhis2.org>
2023-08-22 11:11:57 +0200

    create IconResource

--- designsystem/src/commonMain/kotlin/org/hisp/dhis/mobile/ui/designsystem/extensions/IconResources.kt
+++
@@ -1,9 +0,0 @@
-package org.hisp.dhis.mobile.ui.designsystem.extensions
-
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.painter.Painter
-import dev.icerock.moko.resources.ImageResource
-import dev.icerock.moko.resources.compose.painterResource
-
-@Composable
-fun dhis2IconResource(resource: ImageResource): Painter = painterResource(resource)
---
+++ designsystem/src/commonMain/kotlin/org/hisp/dhis/mobile/ui/designsystem/theme/IconResource.kt
@@ -0,0 +1,18 @@
+package org.hisp.dhis.mobile.ui.designsystem.theme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.painter.Painter
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.painterResource
+
+object IconResource {
+
+    /**
+     * DHIS2 icons resources
+     * @param resourceName the name of the icon
+     */
+    @OptIn(ExperimentalResourceApi::class)
+    @Composable
+    fun DHIS2Icon(resourceName: String): Painter =
+        painterResource("drawable/$resourceName.xml")
+}
