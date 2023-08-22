6ad50f83c56bb4f05dd6ad082fcee5d7cf1cd96f
andresmr <andres@dhis2.org>
2023-08-22 10:19:33 +0200

    update to Kotlin 1.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,7 @@
 plugins {
-    kotlin("jvm") apply false
     kotlin("multiplatform") apply false
-    kotlin("android") apply false
     id("com.android.application") apply false
     id("com.android.library") apply false
     id("org.jetbrains.compose") apply false
     id("org.jlleitschuh.gradle.ktlint") version "11.5.0"
-}
-
-buildscript {
-    repositories {
-        mavenLocal()
-    }
-    dependencies {
-        classpath(moko.resourcesGradlePlugin)
-    }
 }
