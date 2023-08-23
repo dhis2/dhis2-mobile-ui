23f95a061b2c0bd1a2cc7ac6f3072606dd46a604
Xavier Molloy <xavi.molloy@gmail.com>
2023-08-23 14:49:55 +0200

    fix import

--- common/src/commonMain/kotlin/org/hisp/dhis/common/App.kt
+++ common/src/commonMain/kotlin/org/hisp/dhis/common/App.kt
@@ -26,6 +26,7 @@ import org.hisp.dhis.common.screens.Components
 import org.hisp.dhis.common.screens.FormsComponentsScreen
 import org.hisp.dhis.common.screens.IconButtonScreen
 import org.hisp.dhis.common.screens.InputScreen
+import org.hisp.dhis.common.screens.LegendScreen
 import org.hisp.dhis.common.screens.NotImplementedScreen
 import org.hisp.dhis.common.screens.ProgressScreen
 import org.hisp.dhis.common.screens.SupportingTextScreen
