8ef0f83626eba14df670f1dd68c0e384df54019a
Xavier Molloy <xavi.molloy@gmail.com>
2023-08-25 12:01:12 +0200

    Refactor ButtonBlock into button

--- designsystem/src/commonMain/kotlin/org/hisp/dhis/mobile/ui/designsystem/component/Button.kt
+++ designsystem/src/commonMain/kotlin/org/hisp/dhis/mobile/ui/designsystem/component/Button.kt
@@ -3,9 +3,14 @@ package org.hisp.dhis.mobile.ui.designsystem.component
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ripple.LocalRippleTheme
 import androidx.compose.material3.Button
@@ -234,4 +239,32 @@ enum class ButtonStyle {
     ELEVATED,
     TONAL,
     KEYBOARDKEY,
+}
+
+/**
+ * DHIS2 ButtonBlock with generic buttons slot.
+ * @param primaryButton Controls first or primary button, if there is
+ * only one it will be centered, otherwise spaced equally
+ * @param secondaryButton Controls the second button to be shown
+ * @param modifier allow a modifier to be passed to the composable.
+ */
+@Composable
+fun ButtonBlock(
+    primaryButton: @Composable (() -> Unit)? = null,
+    secondaryButton: @Composable (() -> Unit)? = null,
+    modifier: Modifier = Modifier
+) {
+    Row(horizontalArrangement = Arrangement.Center, modifier = modifier) {
+        if (secondaryButton != null) {
+            Box(modifier = Modifier.weight(0.5f)) {
+                primaryButton?.invoke()
+            }
+            Spacer(Modifier.size(Spacing.Spacing16))
+            Box(modifier = Modifier.weight(0.5f)) {
+                secondaryButton.invoke()
+            }
+        } else {
+            primaryButton?.invoke()
+        }
+    }
 }
