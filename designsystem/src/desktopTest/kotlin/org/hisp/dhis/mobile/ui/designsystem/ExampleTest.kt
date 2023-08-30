dca7e9d4af96803fa9cced806e0610c40abf7bac
andresmr <andres@dhis2.org>
2023-08-30 08:49:23 +0200

    [ANDROAPP-5450] Add Compose Multiplatform desktopTest

---
+++ designsystem/src/desktopTest/kotlin/org/hisp/dhis/mobile/ui/designsystem/ExampleTest.kt
@@ -0,0 +1,42 @@
+package org.hisp.dhis.mobile.ui.designsystem
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.test.assertTextEquals
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import org.junit.Rule
+import org.junit.Test
+
+class ExampleTest {
+
+    @get:Rule
+    val rule = createComposeRule()
+
+    @Test
+    fun myTest() {
+        rule.setContent {
+            var text by remember { mutableStateOf("Hello") }
+            Text(
+                text = text,
+                modifier = Modifier.testTag("text"),
+            )
+            androidx.compose.material3.Button(
+                onClick = { text = "Compose" },
+                modifier = Modifier.testTag("button"),
+            ) {
+                Text("Click me")
+            }
+        }
+
+        rule.onNodeWithTag("text").assertTextEquals("Hello")
+        rule.onNodeWithTag("button").performClick()
+        rule.onNodeWithTag("text").assertTextEquals("Compose")
+    }
+}
