a0654c9f63363727c61ec651adac2fba4b3089f0
Sasikanth Miriyampalli <hello@sasikanth.dev>
2023-09-19 06:12:38 +0530

    Add a negative metadata avatar sample to scree

--- common/src/commonMain/kotlin/org/hisp/dhis/common/screens/IconCardsScreen.kt
+++ common/src/commonMain/kotlin/org/hisp/dhis/common/screens/IconCardsScreen.kt
@@ -2,6 +2,7 @@ package org.hisp.dhis.common.screens
 
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import org.hisp.dhis.mobile.ui.designsystem.component.AvatarSize
 import org.hisp.dhis.mobile.ui.designsystem.component.ColumnComponentContainer
 import org.hisp.dhis.mobile.ui.designsystem.component.MetadataAvatar
@@ -53,6 +54,28 @@ fun IconCardsScreen() {
                     )
                 },
                 iconTint = SurfaceColor.CustomGreen,
+                size = AvatarSize.Large,
+            )
+        }
+        RowComponentContainer {
+            MetadataAvatar(
+                icon = {
+                    Icon(
+                        painter = provideDHIS2Icon("dhis2_medicines_negative"),
+                        contentDescription = "",
+                    )
+                },
+                iconTint = Color(0xFFE12F58),
+                size = AvatarSize.Normal,
+            )
+            MetadataAvatar(
+                icon = {
+                    Icon(
+                        painter = provideDHIS2Icon("dhis2_medicines_negative"),
+                        contentDescription = "",
+                    )
+                },
+                iconTint = Color(0xFFE12F58),
                 size = AvatarSize.Large,
             )
         }
