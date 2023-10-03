1cb891176becd366c6c718a90b5c0023a1428bbf
= <david.aparicio@dhis2.org>
2023-10-03 09:43:32 +0200

    update: [ANDROAPP-5495] using Button instead of IconButton, pass content as argument.

--- common/src/commonMain/kotlin/org/hisp/dhis/common/screens/ButtonCarouselScreen.kt
+++ common/src/commonMain/kotlin/org/hisp/dhis/common/screens/ButtonCarouselScreen.kt
@@ -1,13 +1,21 @@
 package org.hisp.dhis.common.screens
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.FileDownload
+import androidx.compose.material.icons.filled.DocumentScanner
+import androidx.compose.material.icons.filled.QrCodeScanner
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material.icons.outlined.AddTask
 import androidx.compose.material.icons.outlined.FileDownload
+import androidx.compose.material.icons.outlined.FilterAlt
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import org.hisp.dhis.mobile.ui.designsystem.component.ButtonCarousel
 import org.hisp.dhis.mobile.ui.designsystem.component.CarouselButton
-import org.hisp.dhis.mobile.ui.designsystem.component.CarouselButtonData
 import org.hisp.dhis.mobile.ui.designsystem.component.ColumnComponentContainer
 import org.hisp.dhis.mobile.ui.designsystem.component.RowComponentContainer
 import org.hisp.dhis.mobile.ui.designsystem.component.Title
@@ -34,12 +42,144 @@ fun ButtonCarouselScreen() {
         RowComponentContainer(
             title = "Buttons Carousel",
         ) {
-            ButtonCarousel(
-                listOf(
-                    CarouselButtonData(true, "Label 1", "dhis2_microscope_outline"),
-                    CarouselButtonData(true, "Option 1", "dhis2_nurse_positive"),
-                ),
-            ) {}
+            Column(
+                Modifier.fillMaxWidth(),
+            ) {
+                ButtonCarousel(
+                    carouselButtonList = {
+                        Box(
+                            Modifier.weight(1f),
+                            contentAlignment = Alignment.Center,
+                        ) {
+                            CarouselButton(
+                                enabled = true,
+                                textInput = "Share",
+                                icon = {
+                                    Icon(
+                                        Icons.Filled.Share,
+                                        contentDescription = "",
+                                    )
+                                },
+                                onClick = {},
+                            )
+                        }
+                        Box(
+                            Modifier.weight(1f),
+                            contentAlignment = Alignment.Center,
+                        ) {
+                            CarouselButton(
+                                enabled = true,
+                                textInput = "Scan",
+                                icon = {
+                                    Icon(
+                                        Icons.Filled.QrCodeScanner,
+                                        contentDescription = "",
+                                    )
+                                },
+                                onClick = {},
+                            )
+                        }
+                        Box(
+                            Modifier.weight(1f),
+                            contentAlignment = Alignment.Center,
+                        ) {
+                            CarouselButton(
+                                enabled = true,
+                                textInput = "Download",
+                                icon = {
+                                    Icon(
+                                        imageVector = Icons.Outlined.FileDownload,
+                                        contentDescription = "",
+                                    )
+                                },
+                                onClick = {},
+                            )
+                        }
+                    },
+
+                )
+
+                ButtonCarousel(
+                    carouselButtonList = {
+                        Box(
+                            contentAlignment = Alignment.Center,
+                        ) {
+                            CarouselButton(
+                                enabled = true,
+                                textInput = "Two lines share label",
+                                icon = {
+                                    Icon(
+                                        Icons.Filled.Share,
+                                        contentDescription = "",
+                                    )
+                                },
+                                onClick = {},
+                            )
+                        }
+                        Box(
+                            contentAlignment = Alignment.Center,
+                        ) {
+                            CarouselButton(
+                                enabled = true,
+                                textInput = "Scan Barcode",
+                                icon = {
+                                    Icon(
+                                        Icons.Filled.DocumentScanner,
+                                        contentDescription = "",
+                                    )
+                                },
+                                onClick = {},
+                            )
+                        }
+                        Box(
+                            contentAlignment = Alignment.Center,
+                        ) {
+                            CarouselButton(
+                                enabled = true,
+                                textInput = "Download files",
+                                icon = {
+                                    Icon(
+                                        imageVector = Icons.Outlined.FileDownload,
+                                        contentDescription = "",
+                                    )
+                                },
+                                onClick = {},
+                            )
+                        }
+                        Box(
+                            contentAlignment = Alignment.Center,
+                        ) {
+                            CarouselButton(
+                                enabled = true,
+                                textInput = "Filter list",
+                                icon = {
+                                    Icon(
+                                        imageVector = Icons.Outlined.FilterAlt,
+                                        contentDescription = "",
+                                    )
+                                },
+                                onClick = {},
+                            )
+                        }
+                        Box(
+                            contentAlignment = Alignment.Center,
+                        ) {
+                            CarouselButton(
+                                enabled = true,
+                                textInput = "Add task to done",
+                                icon = {
+                                    Icon(
+                                        imageVector = Icons.Outlined.AddTask,
+                                        contentDescription = "",
+                                    )
+                                },
+                                onClick = {},
+                            )
+                        }
+                    },
+
+                )
+            }
         }
     }
 }
--- designsystem/src/commonMain/kotlin/org/hisp/dhis/mobile/ui/designsystem/component/ButtonCarousel.kt
+++ designsystem/src/commonMain/kotlin/org/hisp/dhis/mobile/ui/designsystem/component/ButtonCarousel.kt
@@ -1,13 +1,19 @@
 package org.hisp.dhis.mobile.ui.designsystem.component
 
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.ripple.LocalRippleTheme
-import androidx.compose.material3.FilledIconButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,9 +22,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
-import org.hisp.dhis.mobile.ui.designsystem.resource.provideDHIS2Icon
-import org.hisp.dhis.mobile.ui.designsystem.theme.InternalSizeValues
 import org.hisp.dhis.mobile.ui.designsystem.theme.Ripple
+import org.hisp.dhis.mobile.ui.designsystem.theme.Shape
 import org.hisp.dhis.mobile.ui.designsystem.theme.Spacing
 import org.hisp.dhis.mobile.ui.designsystem.theme.TextColor
 import org.hisp.dhis.mobile.ui.designsystem.theme.hoverPointerIcon
@@ -33,23 +38,24 @@ fun CarouselButton(
     (() -> Unit),
 ) {
     CompositionLocalProvider(LocalRippleTheme provides Ripple.CustomDHISRippleTheme) {
-        FilledIconButton(
+        Button(
             onClick = onClick,
             modifier = modifier
-                .size(width = Spacing.Spacing80, height = Spacing.Spacing80)
+                .padding(top = Spacing.Spacing4)
+                .width(Spacing.Spacing80)
+                .wrapContentSize()
                 .hoverPointerIcon(enabled),
+            shape = Shape.Full,
             enabled = enabled,
-            colors = IconButtonDefaults.iconButtonColors(Color.Transparent, TextColor.OnSurfaceVariant, Color.Transparent, TextColor.OnDisabledSurface),
+            colors = ButtonDefaults.buttonColors(Color.Transparent, TextColor.OnSurfaceVariant, Color.Transparent, TextColor.OnDisabledSurface),
         ) {
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center,
-                modifier = Modifier.size(InternalSizeValues.Size64, InternalSizeValues.Size52),
             ) {
-                Spacer(Modifier.size(Spacing.Spacing4))
                 icon.invoke()
                 Spacer(Modifier.size(Spacing.Spacing8))
-                Text(textInput, textAlign = TextAlign.Center, style = MaterialTheme.typography.labelSmall)
+                Text(textInput, style = MaterialTheme.typography.labelSmall, textAlign = TextAlign.Center, maxLines = 2)
             }
         }
     }
@@ -57,28 +63,14 @@ fun CarouselButton(
 
 @Composable
 fun ButtonCarousel(
-    carouselButtonList: List<CarouselButtonData>,
-    onClick: () -> Unit,
+    carouselButtonList: @Composable () -> Unit,
 ) {
-    FlowRowComponentsContainer(
-        content = {
-            carouselButtonList.map {
-                CarouselButton(
-                    enabled = it.enabled,
-                    textInput = it.textInput,
-                    icon = {
-                        Icon(
-                            modifier = Modifier.size(Spacing.Spacing24),
-                            painter = provideDHIS2Icon(it.iconResource),
-                            contentDescription = "",
-                        )
-                    },
-                    onClick = onClick,
-                )
-            }
-        },
-        spacing = Spacing.Spacing16,
-    )
+    Row(
+        Modifier
+            .fillMaxSize()
+            .horizontalScroll(rememberScrollState()),
+        horizontalArrangement = Arrangement.Center,
+    ) {
+        carouselButtonList.invoke()
+    }
 }
-
-data class CarouselButtonData(val enabled: Boolean, val textInput: String, val iconResource: String)
