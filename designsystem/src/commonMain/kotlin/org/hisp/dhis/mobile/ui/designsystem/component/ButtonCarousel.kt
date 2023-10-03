d9799dc217fed26f2953e1bfd38cbae15db97cf8
= <david.aparicio@dhis2.org>
2023-10-03 13:04:38 +0200

    delete: [ANDROAPP-5495] Removed unused import

--- designsystem/src/commonMain/kotlin/org/hisp/dhis/mobile/ui/designsystem/component/ButtonCarousel.kt
+++ designsystem/src/commonMain/kotlin/org/hisp/dhis/mobile/ui/designsystem/component/ButtonCarousel.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.ripple.LocalRippleTheme
 import androidx.compose.material3.Button
