3f4213882f4343ffb1bad279685f151c703c07c7
= <david.aparicio@dhis2.org>
2023-10-27 17:51:12 +0200

    update: [ANDROAPP-5635] onClear added

--- designsystem/src/commonMain/kotlin/org/hisp/dhis/mobile/ui/designsystem/component/SearchBar.kt
+++ designsystem/src/commonMain/kotlin/org/hisp/dhis/mobile/ui/designsystem/component/SearchBar.kt
@@ -1,18 +1,11 @@
 package org.hisp.dhis.mobile.ui.designsystem.component
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.outlined.Cancel
+import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.ListItem
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,12 +13,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.semantics.isTraversalGroup
-import androidx.compose.ui.semantics.semantics
-import androidx.compose.ui.semantics.traversalIndex
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.platform.testTag
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -34,6 +23,7 @@ fun SearchBar(
     onActiveChange: () -> Unit = {},
     onSearch: (String) -> Unit = {},
     onQueryChange: (String) -> Unit = {},
+    onClear: () -> Unit = {},
     modifier: Modifier = Modifier
 ) {
     var text by rememberSaveable { mutableStateOf("") }
@@ -56,15 +46,32 @@ fun SearchBar(
             placeholder = { Text(placeHolderText) },
             trailingIcon = {
                 if (text != "") {
-                    Icon(
-                        imageVector = Icons.Outlined.Cancel,
-                        contentDescription = "cancel icon",
+
+                    IconButton(
+                        modifier = Modifier.testTag(INPUT_FILE_TEST_TAG + CLEAR_BUTTON_TEST_TAG),
+                        icon = {
+                            Icon(
+                                imageVector = Icons.Outlined.Cancel,
+                                contentDescription = "cancel button",
+                            )
+                        },
+                        onClick = {
+                            text = ""
+                            onClear.invoke()
+                        },
                     )
                 } else {
-                    Icon(
-                        Icons.Default.Search,
-                        contentDescription = "search icon"
+                    IconButton(
+                        modifier = Modifier.testTag(INPUT_FILE_TEST_TAG + CLEAR_BUTTON_TEST_TAG),
+                        icon = {
+                            Icon(
+                                imageVector = Icons.Outlined.Search,
+                                contentDescription = "search button",
+                            )
+                        },
+                        onClick = {},
                     )
+
                 }},
             content = {}
         )
