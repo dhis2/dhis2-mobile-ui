3a99c5e3f44e983b01afd4789d0540c997380e73
= <david.aparicio@dhis2.org>
2023-11-02 09:42:07 +0100

    update: [ANDROAPP-5635] code formatted

--- common/src/commonMain/kotlin/org/hisp/dhis/common/screens/SearchBarScreen.kt
+++ common/src/commonMain/kotlin/org/hisp/dhis/common/screens/SearchBarScreen.kt
@@ -1,25 +1,17 @@
 package org.hisp.dhis.common.screens
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
 import org.hisp.dhis.mobile.ui.designsystem.component.ColumnComponentContainer
 import org.hisp.dhis.mobile.ui.designsystem.component.SearchBar
 import org.hisp.dhis.mobile.ui.designsystem.component.SubTitle
 import org.hisp.dhis.mobile.ui.designsystem.component.Title
-import org.hisp.dhis.mobile.ui.designsystem.theme.Spacing
 import org.hisp.dhis.mobile.ui.designsystem.theme.TextColor
 
 @Composable
 fun SearchBarScreen() {
-ColumnComponentContainer {
-
+    ColumnComponentContainer {
         Title("Search bar component", textColor = TextColor.OnSurfaceVariant)
         SubTitle("Search bar", textColor = TextColor.OnSurfaceVariant)
 
@@ -29,6 +21,5 @@ ColumnComponentContainer {
         SubTitle("Search bar", textColor = TextColor.OnSurfaceVariant)
 
         SearchBar(text = "Input")
-
     }
 }
--- designsystem/src/commonMain/kotlin/org/hisp/dhis/mobile/ui/designsystem/component/SearchBar.kt
+++ designsystem/src/commonMain/kotlin/org/hisp/dhis/mobile/ui/designsystem/component/SearchBar.kt
@@ -1,22 +1,17 @@
 package org.hisp.dhis.mobile.ui.designsystem.component
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Cancel
 import androidx.compose.material.icons.outlined.Search
-import androidx.compose.material.ripple.LocalRippleTheme
-import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.SearchBar
-import androidx.compose.material3.SearchBarColors
 import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -24,8 +19,6 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.semantics.Role
-import org.hisp.dhis.mobile.ui.designsystem.theme.Ripple
 import org.hisp.dhis.mobile.ui.designsystem.theme.SurfaceColor
 import org.hisp.dhis.mobile.ui.designsystem.theme.TextColor
 
@@ -38,70 +31,68 @@ fun SearchBar(
     onSearch: (String) -> Unit = {},
     onQueryChange: (String) -> Unit = {},
     onClear: () -> Unit = {},
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
 ) {
     var text by rememberSaveable { mutableStateOf(text) }
     var interactionSource = remember { MutableInteractionSource() }
-
+    val isPressed by interactionSource.collectIsPressedAsState()
 
     Box {
-            SearchBar(
-                modifier = modifier.clickable(
-                    interactionSource = interactionSource,
-                    indication = null,
-                    onClick = {}
-                ),
-                query = text,
-                onQueryChange = {
-                    text = it
-                    onQueryChange.invoke(it)
-                },
-                onSearch = {
-                    onSearch(it)
-                },
-                active = false,
-                onActiveChange = {
-                    onActiveChange.invoke()
-                },
-                colors = SearchBarDefaults.colors(containerColor = SurfaceColor.ContainerLow),
-                placeholder = {
-                    Text(
-                        text = placeHolderText,
-                        color = TextColor.OnDisabledSurface
+        SearchBar(
+            modifier = modifier,
+            query = text,
+            onQueryChange = {
+                text = it
+                onQueryChange.invoke(it)
+            },
+            onSearch = {
+                onSearch(it)
+            },
+            active = false,
+            onActiveChange = {
+                onActiveChange.invoke()
+            },
+            colors = if (!isPressed) {
+                SearchBarDefaults.colors(containerColor = SurfaceColor.ContainerLow)
+            } else {
+                SearchBarDefaults.colors(containerColor = SurfaceColor.Container)
+            },
+            placeholder = {
+                Text(
+                    text = placeHolderText,
+                    color = TextColor.OnDisabledSurface,
+                )
+            },
+            trailingIcon = {
+                if (text != "") {
+                    IconButton(
+                        modifier = Modifier.testTag(INPUT_FILE_TEST_TAG + CLEAR_BUTTON_TEST_TAG),
+                        icon = {
+                            Icon(
+                                imageVector = Icons.Outlined.Cancel,
+                                contentDescription = "cancel button",
+                            )
+                        },
+                        onClick = {
+                            text = ""
+                            onClear.invoke()
+                        },
                     )
-                },
-                trailingIcon = {
-                    if (text != "") {
-
-                        IconButton(
-                            modifier = Modifier.testTag(INPUT_FILE_TEST_TAG + CLEAR_BUTTON_TEST_TAG),
-                            icon = {
-                                Icon(
-                                    imageVector = Icons.Outlined.Cancel,
-                                    contentDescription = "cancel button",
-                                )
-                            },
-                            onClick = {
-                                text = ""
-                                onClear.invoke()
-                            },
-                        )
-                    } else {
-                        IconButton(
-                            modifier = Modifier.testTag(INPUT_FILE_TEST_TAG + CLEAR_BUTTON_TEST_TAG),
-                            icon = {
-                                Icon(
-                                    imageVector = Icons.Outlined.Search,
-                                    contentDescription = "search button",
-                                )
-                            },
-                            onClick = {},
-                        )
-
-                    }
-                },
-                interactionSource = interactionSource,
-                content = {}
-            )
+                } else {
+                    IconButton(
+                        modifier = Modifier.testTag(INPUT_FILE_TEST_TAG + CLEAR_BUTTON_TEST_TAG),
+                        icon = {
+                            Icon(
+                                imageVector = Icons.Outlined.Search,
+                                contentDescription = "search button",
+                            )
+                        },
+                        onClick = {},
+                    )
+                }
+            },
+            interactionSource = interactionSource,
+            content = {},
+        )
     }
 }
