05bc4bc4ed86a4648ea1170048a1e30a3e3fd9d2
= <david.aparicio@dhis2.org>
2023-11-03 11:19:02 +0100

    update: [ANDROAPP-5714] title font weight updated

--- designsystem/src/commonMain/kotlin/org/hisp/dhis/mobile/ui/designsystem/component/Sections.kt
+++ designsystem/src/commonMain/kotlin/org/hisp/dhis/mobile/ui/designsystem/component/Sections.kt
@@ -319,7 +319,7 @@ internal fun SectionTitle(
     Text(
         modifier = modifier,
         text = title,
-        style = MaterialTheme.typography.titleLarge,
+        style = MaterialTheme.typography.titleLarge.copy(fontWeight = FontWeight.Medium),
         color = TextColor.OnSurface,
     )
 }
@@ -340,7 +340,7 @@ internal fun CompletionLabel(
         Text(
             modifier = modifier,
             text = "$completedFields/$totalFields",
-            style = MaterialTheme.typography.bodyMedium.copy(fontWeight = FontWeight.Medium),
+            style = MaterialTheme.typography.bodyMedium,
             color = TextColor.OnSurfaceLight,
         )
     }
