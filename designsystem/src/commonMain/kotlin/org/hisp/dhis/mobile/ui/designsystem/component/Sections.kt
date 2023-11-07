3959998de68c2883a63cd9fb698f354ea8925f23
= <david.aparicio@dhis2.org>
2023-11-07 13:22:23 +0100

    update: [ANDROAPP-5714] typography changed

--- designsystem/src/commonMain/kotlin/org/hisp/dhis/mobile/ui/designsystem/component/Sections.kt
+++ designsystem/src/commonMain/kotlin/org/hisp/dhis/mobile/ui/designsystem/component/Sections.kt
@@ -320,7 +320,7 @@ internal fun SectionTitle(
     Text(
         modifier = modifier,
         text = title,
-        style = MaterialTheme.typography.titleLarge.copy(fontWeight = FontWeight.Medium),
+        style = MaterialTheme.typography.titleLarge,
         color = TextColor.OnSurface,
     )
 }
