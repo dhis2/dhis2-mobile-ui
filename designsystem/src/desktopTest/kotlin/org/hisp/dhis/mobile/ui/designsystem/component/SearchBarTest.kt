a3cdf606e8377e88df152981180d0cb2028aef33
DavidAparicioAlbaAsenjo <137989685+DavidAparicioAlbaAsenjo@users.noreply.github.com>
2023-11-10 10:03:10 +0100

    ANDROAPP-5635-mobile-ui-Create-Search-bar-component (#132)
    * add: [ANDROAPP-5635] screen added, component added, search bar component created
    
    * update: [ANDROAPP-5635] onClear added
    
    * update: [ANDROAPP-5635] color modified, text input added
    
    * update: [ANDROAPP-5635] code formatted
    
    * add: [ANDROAPP-5635] tests added, code formatted
    
    * update: [ANDROAPP-5635] search bar is now stateless
    
    * update: [ANDROAPP-5635] various changes
    
    * update: [ANDROAPP-5635] code formatted
    
    * update: [ANDROAPP-5635] tests fixed

---
+++ designsystem/src/desktopTest/kotlin/org/hisp/dhis/mobile/ui/designsystem/component/SearchBarTest.kt
@@ -0,0 +1,62 @@
+package org.hisp.dhis.mobile.ui.designsystem.component
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import org.junit.Rule
+import org.junit.Test
+
+class SearchBarTest {
+    @get:Rule
+    val rule = createComposeRule()
+
+    @Test
+    fun shouldDisplayTextCorrectly() {
+        rule.setContent {
+            SearchBar(text = "Input")
+        }
+        rule.onNodeWithText("Input").assertExists()
+    }
+
+    @Test
+    fun shouldDisplayClearButton() {
+        rule.setContent {
+            SearchBar(text = "Input")
+        }
+        rule.onNodeWithTag("CANCEL_BUTTON").assertExists()
+        rule.onNodeWithTag("SEARCH_BUTTON").assertDoesNotExist()
+    }
+
+    @Test
+    fun shouldDisplaySearchButton() {
+        rule.setContent {
+            SearchBar()
+        }
+        rule.onNodeWithTag("CANCEL_BUTTON").assertDoesNotExist()
+        rule.onNodeWithTag("SEARCH_BUTTON").assertExists()
+    }
+
+    @Test
+    fun textDisappearsAfterPressCancelButton() {
+        rule.setContent {
+            var text by rememberSaveable {
+                mutableStateOf("Input")
+            }
+            SearchBar(
+                text = text,
+                onQueryChange = {
+                    text = it
+                },
+            )
+        }
+        rule.onNodeWithText("Input").assertExists()
+        rule.onNodeWithTag("CANCEL_BUTTON").performClick()
+        rule.onNodeWithText("").assertExists()
+        rule.onNodeWithTag("SEARCH_BUTTON").assertExists()
+    }
+}
