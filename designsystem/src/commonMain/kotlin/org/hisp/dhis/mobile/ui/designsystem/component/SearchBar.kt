fc621489a7f79eb63d9f4cacf4e4d9ffe1275ecd
DavidAparicioAlbaAsenjo <137989685+DavidAparicioAlbaAsenjo@users.noreply.github.com>
2023-11-10 10:03:10 +0100

    ANDROAPP-5635-mobile-ui-Create-Search-bar-component (#132)
    * add: [ANDROAPP-5635] screen added, component added, search bar component created
    
    * update: [ANDROAPP-5635] onClear added
    
    * update: [ANDROAPP-5635] color modified, text input added
    
    * update: [ANDROAPP-5635] code formatted
    
    * add: [ANDROAPP-5635] tests added, code formatted
    
    * update: [ANDROAPP-5635] search bar is now stateless
    
    * update: [ANDROAPP-5635] various changes
    
    * update: [ANDROAPP-5635] code formatted
    
    * update: [ANDROAPP-5635] tests fixed

---
+++ designsystem/src/commonMain/kotlin/org/hisp/dhis/mobile/ui/designsystem/component/SearchBar.kt
@@ -0,0 +1,83 @@
+package org.hisp.dhis.mobile.ui.designsystem.component
+
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Cancel
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import org.hisp.dhis.mobile.ui.designsystem.theme.SurfaceColor
+import org.hisp.dhis.mobile.ui.designsystem.theme.TextColor
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchBar(
+    text: String = "",
+    placeHolderText: String = "Search",
+    onActiveChange: (Boolean) -> Unit = {},
+    onSearch: (String) -> Unit = {},
+    onQueryChange: (String) -> Unit = {},
+    modifier: Modifier = Modifier,
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
+
+    SearchBar(
+        modifier = modifier.testTag("SEARCH_INPUT"),
+        query = text,
+        onQueryChange = onQueryChange,
+        onSearch = onSearch,
+        active = false,
+        onActiveChange = onActiveChange,
+        colors = if (!isPressed) {
+            SearchBarDefaults.colors(containerColor = SurfaceColor.ContainerLow)
+        } else {
+            SearchBarDefaults.colors(containerColor = SurfaceColor.Container)
+        },
+        placeholder = {
+            Text(
+                text = placeHolderText,
+                color = TextColor.OnDisabledSurface,
+            )
+        },
+        trailingIcon = {
+            if (text != "") {
+                IconButton(
+                    modifier = Modifier.testTag("CANCEL_BUTTON"),
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Outlined.Cancel,
+                            contentDescription = "cancel button",
+                        )
+                    },
+                    onClick = {
+                        onQueryChange.invoke("")
+                    },
+                )
+            } else {
+                IconButton(
+                    modifier = Modifier.testTag("SEARCH_BUTTON"),
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Outlined.Search,
+                            contentDescription = "search button",
+                        )
+                    },
+                    onClick = {},
+                )
+            }
+        },
+        interactionSource = interactionSource,
+        content = {},
+    )
+}
