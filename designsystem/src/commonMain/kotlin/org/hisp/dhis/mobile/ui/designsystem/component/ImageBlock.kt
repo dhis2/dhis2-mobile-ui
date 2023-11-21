cac05d979c755b82459d4501703c321686c73c65
Siddharth Agarwal <sagarwal@rtsl.org>
2023-11-21 20:23:52 +0530

    add logic to open image in fullscreen

--- designsystem/src/commonMain/kotlin/org/hisp/dhis/mobile/ui/designsystem/component/ImageBlock.kt
+++ designsystem/src/commonMain/kotlin/org/hisp/dhis/mobile/ui/designsystem/component/ImageBlock.kt
@@ -1,5 +1,6 @@
 package org.hisp.dhis.mobile.ui.designsystem.component
 
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -12,7 +13,10 @@ import androidx.compose.material.icons.outlined.FileDownload
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.produceState
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -22,28 +26,34 @@ import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import org.hisp.dhis.mobile.ui.designsystem.component.internal.image.FullScreenImage
 import org.hisp.dhis.mobile.ui.designsystem.theme.Radius
 import org.hisp.dhis.mobile.ui.designsystem.theme.Spacing
 import java.io.IOException
 
 /**
  * DHIS2 Image Block. Wraps compose [Image].
+ * @param title controls the text to be shown for the title
  * @param load to load an image stored in the resource, device memory or from network
  * we can use loadPainter, loadImageBitmap, loadSvgPainter or loadXmlImageVector
  * @param painterFor is a composable function which controls how to paint the load param,
  * @param modifier allows a modifier to be passed externally
  * @param downloadButtonVisible controls whether the download button is visible or not
- * @param onClick is a callback to notify when the download button is clicked.
+ * @param onDownloadButtonClick is a callback to notify when the download button is clicked.
+ * @param onShareButtonClick is a callback to notify when the share button is clicked.
  */
 @Composable
 fun <T> ImageBlock(
+    title: String,
     load: suspend () -> T,
     painterFor: @Composable (T) -> Painter,
     modifier: Modifier = Modifier,
     downloadButtonVisible: Boolean = true,
-    onImageClick: () -> Unit,
-    onClick: () -> Unit,
+    onDownloadButtonClick: () -> Unit,
+    onShareButtonClick: () -> Unit,
 ) {
+    var isFullScreen by remember { mutableStateOf(false) }
+
     val image: T? by produceState<T?>(null) {
         value = withContext(Dispatchers.IO) {
             try {
@@ -55,35 +65,51 @@ fun <T> ImageBlock(
     }
 
     if (image != null) {
-        Box(
-            modifier = modifier
-                .padding(vertical = Spacing.Spacing8)
-                .testTag("IMAGE_BLOCK_CONTAINER"),
-        ) {
-            Image(
+        if (isFullScreen) {
+            FullScreenImage(
+                title = title,
                 painter = painterFor(image!!),
-                contentDescription = null,
-                contentScale = ContentScale.Crop,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .clip(shape = RoundedCornerShape(Radius.S))
-                    .height(160.dp)
-                    .clickable { onImageClick.invoke() },
+                modifier = modifier,
+                onDismiss = {
+                    isFullScreen = !isFullScreen
+                },
+                onDownloadButtonCLick = onDownloadButtonClick,
+                onShareButtonClick = onShareButtonClick
             )
-            if (downloadButtonVisible) {
-                SquareIconButton(
-                    enabled = true,
+        } else {
+            Box(
+                modifier = modifier
+                    .padding(vertical = Spacing.Spacing8)
+                    .testTag("IMAGE_BLOCK_CONTAINER"),
+            ) {
+                Image(
+                    painter = painterFor(image!!),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
                     modifier = Modifier
-                        .align(Alignment.BottomEnd)
-                        .padding(Spacing.Spacing4),
-                    icon = {
-                        Icon(
-                            imageVector = Icons.Outlined.FileDownload,
-                            contentDescription = "File download Button",
-                        )
-                    },
-                ) {
-                    onClick.invoke()
+                        .fillMaxWidth()
+                        .clip(shape = RoundedCornerShape(Radius.S))
+                        .height(160.dp)
+                        .clickable {
+                            isFullScreen = !isFullScreen
+                        }
+                        .animateContentSize(),
+                )
+                if (downloadButtonVisible) {
+                    SquareIconButton(
+                        enabled = true,
+                        modifier = Modifier
+                            .align(Alignment.BottomEnd)
+                            .padding(Spacing.Spacing4),
+                        icon = {
+                            Icon(
+                                imageVector = Icons.Outlined.FileDownload,
+                                contentDescription = "File download Button",
+                            )
+                        },
+                    ) {
+                        onDownloadButtonClick.invoke()
+                    }
                 }
             }
         }
