e08981824ba67b22c160710edafc49655a34c29c
Siddharth Agarwal <sagarwal@rtsl.org>
2023-11-23 17:24:22 +0530

    refactor screen component

--- common/src/commonMain/kotlin/org/hisp/dhis/common/screens/FullScreenImageScreen.kt
+++ common/src/commonMain/kotlin/org/hisp/dhis/common/screens/FullScreenImageScreen.kt
@@ -10,11 +10,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import org.hisp.dhis.mobile.ui.designsystem.component.ColumnComponentContainer
 import org.hisp.dhis.mobile.ui.designsystem.component.ImageBlock
-import org.hisp.dhis.mobile.ui.designsystem.component.SubTitle
-import org.hisp.dhis.mobile.ui.designsystem.component.Title
 import org.hisp.dhis.mobile.ui.designsystem.component.internal.image.FullScreenImage
 import org.hisp.dhis.mobile.ui.designsystem.resource.provideDHIS2Icon
-import org.hisp.dhis.mobile.ui.designsystem.theme.TextColor
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 
@@ -22,10 +19,6 @@ import org.jetbrains.compose.resources.painterResource
 @Composable
 fun FullScreenImageScreen() {
     ColumnComponentContainer {
-        Title("Input Image", textColor = TextColor.OnSurfaceVariant)
-
-        SubTitle("Basic Input Image ", textColor = TextColor.OnSurfaceVariant)
-
         val sampleImage1 = painterResource("drawable/sample.png")
         ImageBlock(
             title = "Image",
@@ -44,26 +37,25 @@ fun FullScreenImageScreen() {
             onShareButtonClick = {},
         )
 
-        var showFullscreenIcon by remember { mutableStateOf(false) }
+        var showFullscreenDhis2Icon by remember { mutableStateOf(false) }
         val iconPainter = provideDHIS2Icon("dhis2_microscope_outline")
         Icon(
             painter = iconPainter,
             contentDescription = null,
             modifier = Modifier.clickable {
-                showFullscreenIcon = !showFullscreenIcon
+                showFullscreenDhis2Icon = !showFullscreenDhis2Icon
             }
         )
-        if (showFullscreenIcon) {
+        if (showFullscreenDhis2Icon) {
             FullScreenImage(
                 painter = iconPainter,
                 title = "DHIS2 Icon",
                 onDismiss = {
-                    showFullscreenIcon = !showFullscreenIcon
+                    showFullscreenDhis2Icon = !showFullscreenDhis2Icon
                 },
                 onShareButtonClick = {},
                 onDownloadButtonCLick = {}
             )
         }
     }
-
 }
