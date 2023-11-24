2c35ce41ad3c9ac0e6e8d0021ea8719323ec491a
Sasikanth Miriyampalli <hello@sasikanth.dev>
2023-11-24 16:53:35 +0530

    ANDROAPP-5644-mobile-ui-update-dropdown-component (#155)
    * Refactor dropdown component
    
    - Bring dropdown menu inside the component
    - Show bottom sheet menu if there are more than 7 items
    - Change input dropdown function signature
    
    * Use `MaterialTheme` body large as base text style for `bodyLargeBold`
    
    * Add corner radius to selected background
    
    * Update gitignore
    
    * Update dropdown menu design
    
    * Add support for search dropdown items shown in bottom sheet
    
    * Show no results found text in bottom sheet dropdown if there are no results after search
    
    * Add config to hide search bar in input dropdown component bottom sheet
    
    * Rename `InputField` to `DropdownInputField`

--- designsystem/src/commonMain/kotlin/org/hisp/dhis/mobile/ui/designsystem/component/InputDropDown.kt
+++ designsystem/src/commonMain/kotlin/org/hisp/dhis/mobile/ui/designsystem/component/InputDropDown.kt
@@ -1,24 +1,45 @@
 package org.hisp.dhis.mobile.ui.designsystem.component
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.ArrowDropDown
 import androidx.compose.material.icons.outlined.Cancel
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import org.hisp.dhis.mobile.ui.designsystem.theme.DHIS2SCustomTextStyles
+import org.hisp.dhis.mobile.ui.designsystem.theme.Spacing.Spacing16
+import org.hisp.dhis.mobile.ui.designsystem.theme.Spacing.Spacing8
+import org.hisp.dhis.mobile.ui.designsystem.theme.SurfaceColor
 import org.hisp.dhis.mobile.ui.designsystem.theme.TextColor
+
+private const val MAX_DROPDOWN_ITEMS = 6
 
 /**
  * DHIS2 Input dropdown. Wraps DHIS · [InputShell].
@@ -33,22 +54,165 @@ import org.hisp.dhis.mobile.ui.designsystem.theme.TextColor
  * item is focused
  * @param modifier allows a modifier to be passed externally
  * @param onResetButtonClicked callback to when reset button is clicked
- * @param onArrowDropDownButtonClicked callback to when arrow drop down button is clicked
+ * @param showSearchBar config whether to show search bar in the bottom sheet
  */
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun InputDropDown(
     title: String,
     state: InputShellState,
-    selectedItem: String? = null,
+    dropdownItems: List<DropdownItem>,
+    selectedItem: DropdownItem? = null,
     supportingTextData: List<SupportingTextData>? = null,
     legendData: LegendData? = null,
     isRequiredField: Boolean = false,
     modifier: Modifier = Modifier,
     onFocusChanged: ((Boolean) -> Unit)? = null,
     onResetButtonClicked: () -> Unit,
-    onArrowDropDownButtonClicked: () -> Unit,
+    onItemSelected: (DropdownItem) -> Unit,
+    showSearchBar: Boolean = true,
+    noResultsFoundString: String = "No results found",
 ) {
     val focusRequester = remember { FocusRequester() }
+    var showDropdown by remember { mutableStateOf(false) }
+
+    val inputField: @Composable (modifier: Modifier) -> Unit = { inputModifier ->
+        DropdownInputField(
+            modifier = inputModifier,
+            focusRequester = focusRequester,
+            title = title,
+            state = state,
+            isRequiredField = isRequiredField,
+            expanded = showDropdown,
+            onFocusChanged = onFocusChanged,
+            supportingTextData = supportingTextData,
+            legendData = legendData,
+            selectedItem = selectedItem,
+            onResetButtonClicked = onResetButtonClicked,
+            onDropdownIconClick = {
+                showDropdown = !showDropdown
+            },
+        )
+    }
+
+    if (dropdownItems.size > MAX_DROPDOWN_ITEMS) {
+        Box {
+            inputField(modifier)
+
+            if (showDropdown) {
+                var searchQuery by remember { mutableStateOf("") }
+
+                val filteredOptions =
+                    dropdownItems.filter { it.label.contains(searchQuery, ignoreCase = true) }
+
+                BottomSheetShell(
+                    modifier = Modifier.testTag("INPUT_DROPDOWN_BOTTOM_SHEET"),
+                    title = title,
+                    content = {
+                        Column(
+                            modifier = Modifier
+                                .testTag("INPUT_DROPDOWN_BOTTOM_SHEET_ITEMS")
+                                .padding(top = Spacing8),
+                        ) {
+                            if (filteredOptions.isNotEmpty()) {
+                                filteredOptions.forEachIndexed { index, item ->
+                                    DropdownItem(
+                                        modifier = Modifier.testTag("INPUT_DROPDOWN_BOTTOM_SHEET_ITEM_$index"),
+                                        item = item,
+                                        selected = selectedItem == item,
+                                        contentPadding = PaddingValues(Spacing8),
+                                        onItemClick = {
+                                            onItemSelected(item)
+                                            showDropdown = false
+                                        },
+                                    )
+                                }
+                            } else {
+                                Text(
+                                    text = noResultsFoundString,
+                                    textAlign = TextAlign.Center,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(24.dp),
+                                )
+                            }
+                        }
+                    },
+                    onDismiss = {
+                        showDropdown = false
+                    },
+                    searchQuery = if (showSearchBar) {
+                        searchQuery
+                    } else {
+                        null
+                    },
+                    onSearch = { searchQuery = it },
+                    onSearchQueryChanged = { searchQuery = it },
+                )
+            }
+        }
+    } else {
+        ExposedDropdownMenuBox(
+            expanded = showDropdown,
+            onExpandedChange = { },
+            modifier = Modifier
+                .background(
+                    color = SurfaceColor.SurfaceBright,
+                    shape = RoundedCornerShape(Spacing8),
+                ),
+        ) {
+            inputField(modifier.menuAnchor())
+
+            MaterialTheme(
+                shapes = Shapes(extraSmall = RoundedCornerShape(Spacing8)),
+            ) {
+                ExposedDropdownMenu(
+                    expanded = showDropdown,
+                    onDismissRequest = { showDropdown = false },
+                    modifier = Modifier.background(
+                        color = SurfaceColor.SurfaceBright,
+                        shape = RoundedCornerShape(Spacing8),
+                    ).testTag("INPUT_DROPDOWN_MENU"),
+                ) {
+                    Column(modifier = Modifier.padding(horizontal = Spacing8)) {
+                        dropdownItems.forEachIndexed { index, item ->
+                            DropdownItem(
+                                modifier = Modifier.testTag("INPUT_DROPDOWN_MENU_ITEM_$index"),
+                                item = item,
+                                selected = selectedItem == item,
+                                contentPadding = PaddingValues(
+                                    horizontal = Spacing8,
+                                    vertical = Spacing16,
+                                ),
+                                onItemClick = {
+                                    onItemSelected(item)
+                                    showDropdown = false
+                                },
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun DropdownInputField(
+    title: String,
+    state: InputShellState,
+    isRequiredField: Boolean,
+    expanded: Boolean,
+    focusRequester: FocusRequester,
+    onFocusChanged: ((Boolean) -> Unit)?,
+    supportingTextData: List<SupportingTextData>?,
+    legendData: LegendData?,
+    selectedItem: DropdownItem?,
+    onResetButtonClicked: () -> Unit,
+    onDropdownIconClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
     InputShell(
         modifier = modifier
             .testTag("INPUT_DROPDOWN")
@@ -77,7 +241,7 @@ fun InputDropDown(
                     modifier = Modifier
                         .testTag("INPUT_DROPDOWN_TEXT")
                         .fillMaxWidth(),
-                    text = selectedItem ?: "",
+                    text = selectedItem?.label.orEmpty(),
                     style = MaterialTheme.typography.bodyLarge.copy(
                         color = if (state != InputShellState.DISABLED) {
                             TextColor.OnSurface
@@ -85,8 +249,6 @@ fun InputDropDown(
                             TextColor.OnDisabledSurface
                         },
                     ),
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis,
                 )
                 Box(
                     modifier = Modifier
@@ -96,7 +258,6 @@ fun InputDropDown(
                             enabled = state != InputShellState.DISABLED,
                             onClick = {
                                 focusRequester.requestFocus()
-                                onArrowDropDownButtonClicked.invoke()
                             },
                         ),
                 )
@@ -109,19 +270,16 @@ fun InputDropDown(
                 },
                 enabled = state != InputShellState.DISABLED,
                 icon = {
-                    Icon(
-                        imageVector = Icons.Outlined.ArrowDropDown,
-                        contentDescription = "Dropdown Button",
-                    )
+                    ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
                 },
                 onClick = {
                     focusRequester.requestFocus()
-                    onArrowDropDownButtonClicked.invoke()
+                    onDropdownIconClick()
                 },
             )
         },
         secondaryButton =
-        if (!selectedItem.isNullOrEmpty() && state != InputShellState.DISABLED) {
+        if (selectedItem != null && state != InputShellState.DISABLED) {
             {
                 IconButton(
                     modifier = Modifier.testTag("INPUT_DROPDOWN_RESET_BUTTON"),
@@ -139,3 +297,40 @@ fun InputDropDown(
         },
     )
 }
+
+@Composable
+private fun DropdownItem(
+    item: DropdownItem,
+    contentPadding: PaddingValues,
+    modifier: Modifier = Modifier,
+    selected: Boolean = false,
+    onItemClick: () -> Unit,
+) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(Spacing8))
+            .clickable(onClick = onItemClick)
+            .background(
+                color = if (selected) {
+                    SurfaceColor.PrimaryContainer
+                } else {
+                    Color.Unspecified
+                },
+            )
+            .padding(contentPadding),
+    ) {
+        Text(
+            text = item.label,
+            style = if (selected) {
+                DHIS2SCustomTextStyles.bodyLargeBold
+            } else {
+                MaterialTheme.typography.bodyLarge
+            },
+            color = TextColor.OnSurface,
+        )
+    }
+}
+
+@Immutable
+data class DropdownItem(val label: String)
