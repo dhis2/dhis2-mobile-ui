ad9e5a5b8e4e3295ed6c90c3bd5bd19f6689c8ae
Siddharth Agarwal <sagarwal@rtsl.org>
2024-01-04 12:08:34 +0530

    add `InputChip` component

--- designsystem/src/commonMain/kotlin/org/hisp/dhis/mobile/ui/designsystem/component/InputChip.kt
+++ designsystem/src/commonMain/kotlin/org/hisp/dhis/mobile/ui/designsystem/component/InputChip.kt
@@ -1,13 +1,14 @@
 package org.hisp.dhis.mobile.ui.designsystem.component
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.outlined.Close
 import androidx.compose.material.ripple.LocalRippleTheme
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FilterChip
 import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
@@ -19,6 +20,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.unit.IntOffset
 import org.hisp.dhis.mobile.ui.designsystem.theme.Outline
@@ -28,18 +30,33 @@ import org.hisp.dhis.mobile.ui.designsystem.theme.TextColor
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun FilterChip(
+fun InputChip(
     modifier: Modifier = Modifier,
     label: String,
     selected: Boolean = false,
+    withTrailingIcon: Boolean = false,
+    enabled: Boolean = true,
     onSelected: ((Boolean) -> Unit)? = null,
+    onIconSelected: (() -> Unit)? = null,
     badge: String? = null,
 ) {
     Box(modifier = modifier) {
         CompositionLocalProvider(LocalRippleTheme provides Ripple.CustomDHISRippleTheme) {
-            FilterChip(
-                onClick = { onSelected?.invoke(!selected) },
-                label = { Text(label, color = TextColor.OnSurfaceVariant) },
+            androidx.compose.material3.InputChip(
+                enabled = enabled,
+                onClick = {
+                    onSelected?.invoke(!selected)
+                },
+                label = {
+                    Text(
+                        label,
+                        color = if (enabled) {
+                            TextColor.OnSurfaceVariant
+                        } else {
+                            TextColor.OnDisabledSurface
+                        }
+                    )
+                },
                 selected = selected,
                 colors = FilterChipDefaults.filterChipColors(
                     containerColor = SurfaceColor.SurfaceBright,
@@ -47,13 +64,19 @@ fun FilterChip(
                 ),
                 border = FilterChipDefaults.filterChipBorder(
                     borderColor = Outline.Dark,
+                    disabledBorderColor = Outline.Medium,
                 ),
-                leadingIcon = if (selected) {
+                trailingIcon = if (withTrailingIcon && enabled) {
                     {
                         Icon(
-                            imageVector = Icons.Filled.Done,
-                            contentDescription = "Done icon",
-                            modifier = Modifier.size(FilterChipDefaults.IconSize),
+                            imageVector = Icons.Outlined.Close,
+                            tint = TextColor.OnSurfaceVariant,
+                            contentDescription = "Close icon",
+                            modifier = Modifier
+                                .size(FilterChipDefaults.IconSize)
+                                .clickable {
+                                    onIconSelected?.invoke()
+                                },
                         )
                     }
                 } else {
