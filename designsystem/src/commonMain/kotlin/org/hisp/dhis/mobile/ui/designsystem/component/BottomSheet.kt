130ecca761064e00bd65dc836d237a44b083465e
Pablo <pablo@dhis2.org>
2024-03-21 09:34:39 +0100

    remove unused imports
    Signed-off-by: Pablo <pablo@dhis2.org>

--- designsystem/src/commonMain/kotlin/org/hisp/dhis/mobile/ui/designsystem/component/BottomSheet.kt
+++ designsystem/src/commonMain/kotlin/org/hisp/dhis/mobile/ui/designsystem/component/BottomSheet.kt
@@ -1,5 +1,6 @@
 package org.hisp.dhis.mobile.ui.designsystem.component
 
+import org.hisp.dhis.mobile.ui.designsystem.theme.Color as ThemeColor
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.ScrollableState
@@ -11,6 +12,7 @@ import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -26,7 +28,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
@@ -42,13 +43,6 @@ import org.hisp.dhis.mobile.ui.designsystem.theme.Spacing.Spacing24
 import org.hisp.dhis.mobile.ui.designsystem.theme.Spacing.Spacing8
 import org.hisp.dhis.mobile.ui.designsystem.theme.SurfaceColor
 import org.hisp.dhis.mobile.ui.designsystem.theme.TextColor
-import org.hisp.dhis.mobile.ui.designsystem.theme.Color as ThemeColor
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.consumeWindowInsets
-import androidx.compose.foundation.layout.navigationBars
-import androidx.compose.foundation.layout.safeContent
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.foundation.layout.systemBarsPadding
 
 @Composable
 fun BottomSheetHeader(
@@ -62,13 +56,14 @@ fun BottomSheetHeader(
     headerTextAlignment: TextAlign = TextAlign.Center,
     modifier: Modifier = Modifier,
 ) {
-    val horizontalAlignment = if (icon != null || hasSearch) Alignment.CenterHorizontally else Alignment.Start
+    val horizontalAlignment =
+        if (icon != null || hasSearch) Alignment.CenterHorizontally else Alignment.Start
     Column(
         modifier = modifier.padding(horizontal = Spacing24, vertical = Spacing0)
             .fillMaxWidth(),
         horizontalAlignment = horizontalAlignment,
     ) {
-        icon?. let {
+        icon?.let {
             it.invoke()
 
             Spacer(Modifier.size(Spacing.Spacing16))
@@ -191,7 +186,8 @@ fun BottomSheetShell(
                     .background(SurfaceColor.SurfaceBright, Shape.ExtraLargeTop)
                     .padding(top = Spacing24),
             ) {
-                val hasSearch = searchQuery != null && onSearchQueryChanged != null && onSearch != null
+                val hasSearch =
+                    searchQuery != null && onSearchQueryChanged != null && onSearch != null
                 val hasTitle by derivedStateOf { !title.isNullOrBlank() }
                 if (hasTitle) {
                     BottomSheetHeader(
@@ -258,7 +254,11 @@ fun BottomSheetShell(
             }
 
             val shadowModifier = if (canScrollForward && content != null) {
-                Modifier.shadow(elevation = 32.dp, ambientColor = ThemeColor.Blue900, spotColor = ThemeColor.Blue900)
+                Modifier.shadow(
+                    elevation = 32.dp,
+                    ambientColor = ThemeColor.Blue900,
+                    spotColor = ThemeColor.Blue900
+                )
             } else {
                 Modifier
             }
@@ -267,7 +267,12 @@ fun BottomSheetShell(
                 Modifier.fillMaxWidth()
                     .then(shadowModifier)
                     .background(SurfaceColor.SurfaceBright)
-                    .padding(start = Spacing24, top = Spacing24, end = Spacing24, bottom = Spacing24),
+                    .padding(
+                        start = Spacing24,
+                        top = Spacing24,
+                        end = Spacing24,
+                        bottom = Spacing24
+                    ),
                 contentAlignment = Alignment.BottomCenter,
             ) {
                 buttonBlock?.invoke()
