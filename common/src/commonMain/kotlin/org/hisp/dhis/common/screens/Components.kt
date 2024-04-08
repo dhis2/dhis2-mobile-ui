b8a02a8425cc6843fb995d33826e3a3967be4538
Xavier Molloy <xavi.molloy@gmail.com>
2024-04-08 12:31:34 +0200

    feat: [ANDROAPP-5676] remove unnecessary comments

--- common/src/commonMain/kotlin/org/hisp/dhis/common/screens/Components.kt
+++ common/src/commonMain/kotlin/org/hisp/dhis/common/screens/Components.kt
@@ -1,21 +1,21 @@
 package org.hisp.dhis.common.screens
 
 enum class Components(val label: String) {
-    ACTION_INPUTS("Action inputs"), // 14
-    BASIC_TEXT_INPUTS("Basic text Inputs"), // 11
-    BOTTOM_SHEETS("Bottom sheet components"), // 3
-    BUTTONS("Buttons"), // 8
-    CARDS("List Cards and Card details"), // 2
-    CHIPS("Chips"), // 1
-    LEGEND("Legend"), // 1
-    PROGRESS_INDICATOR("Progress Indicator"), // 1
-    SECTIONS("Sections"), // 1
-    SEARCH_BAR("Search Bar"), // 1
-    TOGGLEABLE_INPUTS("Options"), // 9
-    TAGS("Tags"), // 1
-    BADGES("Badges"), // 1
-    METADATA_AVATAR("Metadata Avatar"), // 1
-    INDICATOR("Indicators"), // 1
+    ACTION_INPUTS("Action inputs"),
+    BASIC_TEXT_INPUTS("Basic text Inputs"),
+    BOTTOM_SHEETS("Bottom sheet components"),
+    BUTTONS("Buttons"),
+    CARDS("List Cards and Card details"),
+    CHIPS("Chips"),
+    LEGEND("Legend"),
+    PROGRESS_INDICATOR("Progress Indicator"),
+    SECTIONS("Sections"),
+    SEARCH_BAR("Search Bar"),
+    TOGGLEABLE_INPUTS("Options"),
+    TAGS("Tags"),
+    BADGES("Badges"),
+    METADATA_AVATAR("Metadata Avatar"),
+    INDICATOR("Indicators"),
     PARAMETER_SELECTOR("Parameter selector"),
     NO_COMPONENT_SELECTED("No component selected"),
 }
