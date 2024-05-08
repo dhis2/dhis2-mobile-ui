144b6fa59d1095a847095fe99ea5c6990112e216
andresmr <andres@dhis2.org>
2024-05-08 13:28:34 +0200

    add closeAndReleaseSonatypeStagingRepository

--- .github/workflows/scripts/publish-maven.sh
+++ .github/workflows/scripts/publish-maven.sh
@@ -2,8 +2,8 @@ set -x
 
 branch=$(git rev-parse --abbrev-ref HEAD)
 
-if [ "$branch" = "main-test" ]; then
-  ./gradlew :designsystem:publishToSonatype -PremoveSnapshotSuffix
+if [ "$branch" = "main" ]; then
+  ./gradlew :designsystem:publishToSonatype closeAndReleaseSonatypeStagingRepository -PremoveSnapshotSuffix
 else
   ./gradlew :designsystem:publishToSonatype
 fi
