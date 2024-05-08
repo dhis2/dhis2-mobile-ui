640ada731d45ac82085b5dd97055d7d9ddb7dcc2
Andrés Miguel Rubio <andres@dhis2.org>
2024-05-08 12:29:06 +0200

    remove camelcase (#241)

--- .github/workflows/scripts/publish-maven.sh
+++ .github/workflows/scripts/publish-maven.sh
@@ -3,7 +3,7 @@ set -x
 branch=$(git rev-parse --abbrev-ref HEAD)
 
 if [ "$branch" = "main" ]; then
-  ./gradlew publishToSonatype closeAndReleaseSonatypeStagingRepository -PremoveSnapshotSuffix --project-dir designSystem
+  ./gradlew publishToSonatype closeAndReleaseSonatypeStagingRepository -PremoveSnapshotSuffix --project-dir designsystem
 else
-  ./gradlew publishToSonatype --project-dir designSystem
+  ./gradlew publishToSonatype --project-dir designsystem
 fi
