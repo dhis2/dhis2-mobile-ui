657bd642fc374f8e64b5a77ce27b12ad585dc119
DavidAparicioAlbaAsenjo <137989685+DavidAparicioAlbaAsenjo@users.noreply.github.com>
2024-05-22 10:33:08 +0200

    add: [ANDROAPP-6097] Component, screens and tests added (#246)
    * add: [ANDROAPP-6097] Component, screens and tests added
    
    * add: [ANDROAPP-6097] snapshot image added, code formatted
    
    * update: [ANDROAPP-6097] snapshot test class name modified
    
    * update: [ANDROAPP-6097] test path modified
    
    * remove: [ANDROAPP-6097] import removed
    
    * add: [ANDROAPP-6097] snapshot test modified
    
    * remove: [ANDROAPP-6097] comment removed
    
    * add: [ANDROAPP-6097] snapshot added
    
    * test snapshot
    
    ---------
    
    Co-authored-by: manu <mmmateos@protonmail.com>

---
+++ designsystem/src/androidUnitTest/kotlin/org/hisp/dhis/mobile/ui/designsystem/AssistChipSnapshotTest.kt
@@ -0,0 +1,99 @@
+package org.hisp.dhis.mobile.ui.designsystem
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.AssistChipDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.ui.Modifier
+import org.hisp.dhis.mobile.ui.designsystem.component.AssistChip
+import org.hisp.dhis.mobile.ui.designsystem.component.ColumnComponentContainer
+import org.hisp.dhis.mobile.ui.designsystem.theme.Spacing
+import org.junit.Rule
+import org.junit.Test
+
+class AssistChipSnapshotTest {
+    @get:Rule
+    val paparazzi = paparazzi()
+
+    @Test
+    fun launchAssistChip() {
+        paparazzi.snapshot {
+            ColumnComponentContainer(modifier = Modifier.padding(Spacing.Spacing10)) {
+                AssistChip(
+                    label = "Label",
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Filled.Search,
+                            contentDescription = "search icon",
+                            modifier = Modifier
+                                .size(AssistChipDefaults.IconSize),
+                        )
+                    },
+                    onClick = {},
+                    badge = "3",
+                )
+                AssistChip(
+                    label = "Label",
+                    onClick = {},
+                    badge = "3",
+                )
+                AssistChip(
+                    label = "Label",
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Filled.Search,
+                            contentDescription = "search icon",
+                            modifier = Modifier
+                                .size(AssistChipDefaults.IconSize),
+                        )
+                    },
+                    onClick = {},
+                )
+                AssistChip(
+                    label = "Label",
+                    onClick = {},
+                )
+                AssistChip(
+                    label = "Label",
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Filled.Search,
+                            contentDescription = "search icon",
+                            modifier = Modifier
+                                .size(AssistChipDefaults.IconSize),
+                        )
+                    },
+                    enabled = false,
+                    onClick = {},
+                )
+                AssistChip(
+                    label = "Label",
+                    onClick = {},
+                    enabled = false,
+                )
+                AssistChip(
+                    label = "Label",
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Filled.Search,
+                            contentDescription = "search icon",
+                            modifier = Modifier
+                                .size(AssistChipDefaults.IconSize),
+                        )
+                    },
+                    enabled = false,
+                    onClick = {},
+                    badge = "3",
+                )
+                AssistChip(
+                    label = "Label",
+                    onClick = {},
+                    enabled = false,
+                    badge = "3",
+                )
+            }
+        }
+    }
+}
