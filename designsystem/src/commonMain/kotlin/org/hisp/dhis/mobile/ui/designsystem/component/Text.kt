26060e6600be446679417fe28a2a32848bc77e6a
David Aparicio Alba <david.aparicio@dhis2.org>
2024-05-29 10:17:12 +0200

    delete: [ANDROAPP-5739] ListCardValue composable deleted

--- designsystem/src/commonMain/kotlin/org/hisp/dhis/mobile/ui/designsystem/component/Text.kt
+++ designsystem/src/commonMain/kotlin/org/hisp/dhis/mobile/ui/designsystem/component/Text.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
@@ -209,22 +210,6 @@ internal fun ListCardKey(
                 modifiedText = modifiedText.substring(0, lineIndex).trimEnd() + "...:"
             }
         },
-    )
-}
-
-@Composable
-internal fun ListCardValue(
-    text: String,
-    color: Color,
-    modifier: Modifier = Modifier,
-) {
-    Text(
-        text = text,
-        color = color,
-        style = MaterialTheme.typography.bodyMedium,
-        overflow = TextOverflow.Ellipsis,
-        maxLines = 2,
-        modifier = modifier,
     )
 }
 
