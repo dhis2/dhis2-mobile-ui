22dec35a5b0ffced72218be05a1b157957d30f27
Siddharth Agarwal <sagarwal@rtsl.org>
2024-06-10 18:08:09 +0530

    Fix `InputType` keyboard type
    Removed maxLength from the argument, as the user could have entered the value in the textfield and the onValueChange won't have invoked after maLength which could create bugs.
    Despite added minLength which can be used to disable/enable phone button icon.

--- designsystem/src/commonMain/kotlin/org/hisp/dhis/mobile/ui/designsystem/component/InputPhoneNumber.kt
+++ designsystem/src/commonMain/kotlin/org/hisp/dhis/mobile/ui/designsystem/component/InputPhoneNumber.kt
@@ -19,7 +19,7 @@ import org.hisp.dhis.mobile.ui.designsystem.component.internal.RegExValidations
  * @param title: controls the text to be shown for the title.
  * @param onCallActionClicked: callback to when call phone number button is clicked.
  * @param modifier: allows a modifier to be passed externally.
- * @param maxLength: number of characters/digits that can be entered.
+ * @param minLength: number of characters/digits that needs be entered to enable phone button.
  * @param state: Manages the InputShell state.
  * @param inputStyle: manages the InputShell style.
  * @param legendData: manages the legendComponent.
@@ -41,7 +41,7 @@ fun InputPhoneNumber(
     title: String,
     onCallActionClicked: () -> Unit,
     modifier: Modifier = Modifier,
-    maxLength: Int = 12,
+    minLength: Int = 4,
     state: InputShellState,
     inputStyle: InputStyle = InputStyle.DataInputStyle(),
     legendData: LegendData? = null,
@@ -56,7 +56,7 @@ fun InputPhoneNumber(
     supportingText: List<SupportingTextData>? = emptyList(),
     allowedCharacters: RegExValidations = RegExValidations.PHONE_NUMBER,
 ) {
-    val hasMinimumPhoneNumberInput = inputTextFieldValue?.text.orEmpty().length > 2
+    val hasMinimumPhoneNumberInput = inputTextFieldValue?.text.orEmpty().length >= minLength
     BasicTextInput(
         title = title,
         state = state,
@@ -67,15 +67,11 @@ fun InputPhoneNumber(
         isRequiredField = isRequiredField,
         onNextClicked = onNextClicked,
         onValueChanged = {
-            if ((it.text.length) <= maxLength) {
-                onValueChanged?.invoke(it)
-            } else {
-                // no-op
-            }
+            onValueChanged?.invoke(it)
         },
         keyboardOptions = KeyboardOptions(
             imeAction = imeAction,
-            keyboardType = KeyboardType.Number,
+            keyboardType = KeyboardType.Phone,
         ),
         allowedCharacters = allowedCharacters.regex,
         modifier = modifier,
