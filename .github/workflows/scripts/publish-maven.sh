bd603501913c1317e562c8ad47bf676da4233314
Xavier Molloy <xavi@dhis2.org>
2024-07-04 16:58:21 +0200

    ci: [ANDROAPP-6305] prepare for release publication

--- .github/workflows/scripts/publish-maven.sh
+++ .github/workflows/scripts/publish-maven.sh
@@ -3,7 +3,7 @@ set -x
 branch=$(git rev-parse --abbrev-ref HEAD)
 
 if [ "$branch" = "main" ]; then
-  ./gradlew :designsystem:publishToSonatype
+  ./gradlew :designsystem:publishToSonatype closeAndReleaseSonatypeStagingRepository -PremoveSnapshotSuffix
 else
   ./gradlew :designsystem:publishToSonatype
 fi
