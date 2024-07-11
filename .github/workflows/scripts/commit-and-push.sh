d150f04d14daa9e0e48dbb83dd206d4fca8054a7
Sasikanth Miriyampalli <hello@sasikanth.dev>
2024-07-11 14:39:35 +0530

    Add script to commit and push golden image changes to the branch

---
+++ .github/workflows/scripts/commit-and-push.sh
@@ -0,0 +1,19 @@
+readonly GIT_HAS_CHANGES=1 # 0 = clean, 1 = dirty
+
+git config user.name "GitHub Actions Bot"
+git config user.email "<android@dhis2.org>"
+
+# Following git command checks the git diff of staged files
+# and assigns the exit code to GIT_DIFF_STATUS variable.
+git diff --quiet; GIT_DIFF_STATUS=$?
+
+# If the GIT_DIFF_STATUS is 1 (GIT_HAS_CHANGES) then we know
+# there are changes and we can commit the changes and push
+# them to new branch
+if [[ ${GIT_DIFF_STATUS} -eq ${GIT_HAS_CHANGES} ]]; then
+  git add -A
+  git commit -m "Paparazzi Golden Images"
+  git push
+
+exit 0
+fi
