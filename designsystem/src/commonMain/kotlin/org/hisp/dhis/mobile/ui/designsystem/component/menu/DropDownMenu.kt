18454333c56ba68310c0098141166c8029c13689
Siddharth Agarwal <sagarwal@rtsl.org>
2024-10-04 16:55:09 +0530

    Fix shape reference

--- designsystem/src/commonMain/kotlin/org/hisp/dhis/mobile/ui/designsystem/component/menu/DropDownMenu.kt
+++ designsystem/src/commonMain/kotlin/org/hisp/dhis/mobile/ui/designsystem/component/menu/DropDownMenu.kt
@@ -2,13 +2,13 @@ package org.hisp.dhis.mobile.ui.designsystem.component.menu
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.widthIn
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import org.hisp.dhis.mobile.ui.designsystem.theme.Radius
+import org.hisp.dhis.mobile.ui.designsystem.theme.DHISShapes
+import org.hisp.dhis.mobile.ui.designsystem.theme.Shape
 import org.hisp.dhis.mobile.ui.designsystem.theme.SurfaceColor
 
 @Composable
@@ -20,7 +20,7 @@ fun <T> DropDownMenu(
     onDismissRequest: () -> Unit,
     onItemClick: (T) -> Unit,
 ) {
-    MaterialTheme(shapes = MaterialTheme.shapes.copy(extraSmall = RoundedCornerShape(Radius.S))) {
+    MaterialTheme(shapes = DHISShapes.copy(extraSmall = Shape.Small)) {
         DropdownMenu(
             modifier = modifier
                 .background(SurfaceColor.ContainerLow)
