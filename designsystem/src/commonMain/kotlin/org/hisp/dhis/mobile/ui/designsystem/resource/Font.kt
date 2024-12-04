4de78567f54b5e4848c428665ad08743c3485fda
FerdyRod <ferdy@dhis2.org>
2024-12-04 15:53:29 +0100

    Migrated to new resources api.

--- designsystem/src/commonMain/kotlin/org/hisp/dhis/mobile/ui/designsystem/resource/Font.kt
+++
@@ -1,7 +0,0 @@
-package org.hisp.dhis.mobile.ui.designsystem.resource
-
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.text.font.FontFamily
-
-@Composable
-expect fun provideFontResource(font: String = "roboto_regular"): FontFamily
