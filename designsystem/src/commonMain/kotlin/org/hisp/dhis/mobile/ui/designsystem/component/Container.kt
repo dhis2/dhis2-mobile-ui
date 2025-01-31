8805779a43f3bf5796f9cd38a76f3b00449a7990
Pablo Pajuelo Cabezas <pablo@dhis2.org>
2025-01-31 12:47:13 +0100

    fix tabs review comments
    Signed-off-by: Pablo Pajuelo Cabezas <pablo@dhis2.org>

--- designsystem/src/commonMain/kotlin/org/hisp/dhis/mobile/ui/designsystem/component/Container.kt
+++ designsystem/src/commonMain/kotlin/org/hisp/dhis/mobile/ui/designsystem/component/Container.kt
@@ -39,6 +39,7 @@ fun ColumnScreenContainer(
     modifier: Modifier = Modifier,
     verticalArrangement: Arrangement.Vertical = Arrangement.spacedBy(Spacing.Spacing16),
     horizontalAlignment: Alignment.Horizontal = Alignment.Start,
+    scrollable: Boolean = true,
     content: @Composable (() -> Unit),
 ) {
     Column(
@@ -47,7 +48,7 @@ fun ColumnScreenContainer(
         modifier = modifier
             .fillMaxSize().background(Color.White, Shape.LargeTop)
             .padding(horizontal = Spacing.Spacing16)
-            .verticalScroll(rememberScrollState()),
+            .then(if (scrollable)Modifier.verticalScroll(rememberScrollState())else Modifier),
     ) {
         title?.let {
             Title(title, modifier = Modifier.padding(top = Spacing.Spacing16))
