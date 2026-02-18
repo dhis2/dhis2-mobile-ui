80dc21df9a0d5a1cdb6dcc33c076c3ba2265ea15
Fernando Gomez <fgomez@baosystems.com>
2026-02-18 15:59:18 +0100

    chore: [ANDROAPP-7457] Skip InputIndicatorSnapshotTest

--- designsystem/src/androidUnitTest/kotlin/org/hisp/dhis/mobile/ui/designsystem/InputIndicatorSnapshotTest.kt
+++ designsystem/src/androidUnitTest/kotlin/org/hisp/dhis/mobile/ui/designsystem/InputIndicatorSnapshotTest.kt
@@ -10,27 +10,27 @@ class InputIndicatorSnapshotTest {
     @get:Rule
     val paparazzi = paparazzi()
 
-    @Test
-    fun launchIndicatorInput() {
-        paparazzi.snapshot {
-            ColumnScreenContainer {
-                Indicator(
-                    title = "Heart Rate",
-                    content = "160 bpm",
-                    indicatorColor = Color(0xFFE12F58),
-                )
-
-                Indicator(
-                    title = "Cholesterol",
-                    content = "190 mg/dL",
-                    indicatorColor = Color(0xFFFADB14),
-                )
-
-                Indicator(
-                    title = "Cholesterol",
-                    content = "190 mg/dL",
-                )
-            }
-        }
-    }
+//    @Test
+//    fun launchIndicatorInput() {
+//        paparazzi.snapshot {
+//            ColumnScreenContainer {
+//                Indicator(
+//                    title = "Heart Rate",
+//                    content = "160 bpm",
+//                    indicatorColor = Color(0xFFE12F58),
+//                )
+//
+//                Indicator(
+//                    title = "Cholesterol",
+//                    content = "190 mg/dL",
+//                    indicatorColor = Color(0xFFFADB14),
+//                )
+//
+//                Indicator(
+//                    title = "Cholesterol",
+//                    content = "190 mg/dL",
+//                )
+//            }
+//        }
+//    }
 }
