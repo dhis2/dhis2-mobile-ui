e83ee709ca55c3474e2efb6f179e6fdc20f678c8
Fernando Gomez <fgomez@baosystems.com>
2026-02-18 09:07:24 +0100

    fix: [ANDROAPP-7457] Code quality check

--- designsystem/src/commonMain/kotlin/org/hisp/dhis/mobile/ui/designsystem/component/Indicator.kt
+++ designsystem/src/commonMain/kotlin/org/hisp/dhis/mobile/ui/designsystem/component/Indicator.kt
@@ -49,141 +49,156 @@ fun Indicator(
     modifier: Modifier = Modifier,
     indicatorColor: Color = SurfaceColor.ContainerHigh,
 ) {
-    val backgroundColor = indicatorColor.copy(
-        alpha = if(indicatorColor == SurfaceColor.ContainerHigh) 0.2f else 0.1f
-    )
+    val backgroundColor =
+        indicatorColor.copy(
+            alpha = if (indicatorColor == SurfaceColor.ContainerHigh) 0.2f else 0.1f,
+        )
 
     DHIS2Theme {
-        val mdTypographyKey = markdownTypography(
-            h1 = MaterialTheme.typography.titleLarge.copy(
-                fontSize = 24.sp
-            ),
-            h2 = MaterialTheme.typography.titleLarge.copy(
-                fontSize = 22.sp,
-                lineHeight = 26.sp
-            ),
-            h3 = MaterialTheme.typography.titleLarge.copy(
-                fontSize = 20.sp,
-                lineHeight = 24.sp
-            ),
-            h4 = MaterialTheme.typography.titleLarge.copy(
-                fontSize = 18.sp,
-                lineHeight = 22.sp
-            ),
-            h5 = MaterialTheme.typography.titleLarge.copy(
-                fontSize = 16.sp,
-                lineHeight = 20.sp
-            ),
-            h6 = MaterialTheme.typography.titleLarge.copy(
-                fontSize = 14.sp,
-                lineHeight = 20.sp
-            ),
-            paragraph = MaterialTheme.typography.bodyMedium.copy(
-                letterSpacing = 0.sp
-            ),
-        )
+        val mdTypographyKey =
+            markdownTypography(
+                h1 =
+                    MaterialTheme.typography.titleLarge.copy(
+                        fontSize = 24.sp,
+                    ),
+                h2 =
+                    MaterialTheme.typography.titleLarge.copy(
+                        fontSize = 22.sp,
+                        lineHeight = 26.sp,
+                    ),
+                h3 =
+                    MaterialTheme.typography.titleLarge.copy(
+                        fontSize = 20.sp,
+                        lineHeight = 24.sp,
+                    ),
+                h4 =
+                    MaterialTheme.typography.titleLarge.copy(
+                        fontSize = 18.sp,
+                        lineHeight = 22.sp,
+                    ),
+                h5 =
+                    MaterialTheme.typography.titleLarge.copy(
+                        fontSize = 16.sp,
+                        lineHeight = 20.sp,
+                    ),
+                h6 =
+                    MaterialTheme.typography.titleLarge.copy(
+                        fontSize = 14.sp,
+                        lineHeight = 20.sp,
+                    ),
+                paragraph =
+                    MaterialTheme.typography.bodyMedium.copy(
+                        letterSpacing = 0.sp,
+                    ),
+            )
 
-        val mdTypographyValue = markdownTypography(
-            h1 = MaterialTheme.typography.titleLarge.copy(
-                fontSize = 24.sp
-            ),
-            h2 = MaterialTheme.typography.titleLarge.copy(
-                fontSize = 22.sp,
-                lineHeight = 26.sp
-            ),
-            h3 = MaterialTheme.typography.titleLarge.copy(
-                fontSize = 20.sp,
-                lineHeight = 24.sp
-            ),
-            h4 = MaterialTheme.typography.titleLarge.copy(
-                fontSize = 18.sp,
-                lineHeight = 22.sp
-            ),
-            h5 = MaterialTheme.typography.titleLarge.copy(
-                fontSize = 16.sp,
-                lineHeight = 20.sp
-            ),
-            h6 = MaterialTheme.typography.titleLarge.copy(
-                fontSize = 14.sp,
-                lineHeight = 20.sp
-            ),
-            paragraph = MaterialTheme.typography.bodyLarge.copy(
-                letterSpacing = 0.sp
-            ),
-        )
+        val mdTypographyValue =
+            markdownTypography(
+                h1 =
+                    MaterialTheme.typography.titleLarge.copy(
+                        fontSize = 24.sp,
+                    ),
+                h2 =
+                    MaterialTheme.typography.titleLarge.copy(
+                        fontSize = 22.sp,
+                        lineHeight = 26.sp,
+                    ),
+                h3 =
+                    MaterialTheme.typography.titleLarge.copy(
+                        fontSize = 20.sp,
+                        lineHeight = 24.sp,
+                    ),
+                h4 =
+                    MaterialTheme.typography.titleLarge.copy(
+                        fontSize = 18.sp,
+                        lineHeight = 22.sp,
+                    ),
+                h5 =
+                    MaterialTheme.typography.titleLarge.copy(
+                        fontSize = 16.sp,
+                        lineHeight = 20.sp,
+                    ),
+                h6 =
+                    MaterialTheme.typography.titleLarge.copy(
+                        fontSize = 14.sp,
+                        lineHeight = 20.sp,
+                    ),
+                paragraph =
+                    MaterialTheme.typography.bodyLarge.copy(
+                        letterSpacing = 0.sp,
+                    ),
+            )
 
-        val markdownDimensions = markdownDimens(
-            tableCellPadding = 8.dp
-        )
+        val markdownDimensions =
+            markdownDimens(
+                tableCellPadding = 8.dp,
+            )
 
-        val padding = markdownPadding(
-            listItemTop = 0.dp,
-            listItemBottom = 0.dp,
-        )
+        val padding =
+            markdownPadding(
+                listItemTop = 0.dp,
+                listItemBottom = 0.dp,
+            )
 
         val components = markdownComponents()
 
-
-
         Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .clip(RoundedCornerShape(Spacing.Spacing8))
-                .background(Color.White)
-        ){
+            modifier =
+                Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(Spacing.Spacing8))
+                    .background(Color.White),
+        ) {
             Row(
                 modifier =
                     modifier
                         .fillMaxWidth()
                         .clip(RoundedCornerShape(Spacing.Spacing8))
                         .background(backgroundColor)
-                        .drawBehind{
+                        .drawBehind {
                             drawRect(
                                 color = indicatorColor.copy(),
                                 topLeft = Offset(size.width - Spacing.Spacing16.toPx(), 0f),
-                                size = Size(Spacing.Spacing16.toPx(), size.height)
+                                size = Size(Spacing.Spacing16.toPx(), size.height),
                             )
-                        }
-            )
-            {
-
+                        },
+            ) {
                 BoxWithConstraints(
-                    modifier = Modifier.fillMaxWidth().padding(end = Spacing.Spacing16)
-                ){
+                    modifier = Modifier.fillMaxWidth().padding(end = Spacing.Spacing16),
+                ) {
                     val halfWidth = (maxWidth - Spacing.Spacing16) / 2
 
                     Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(
-                                horizontal = Spacing.Spacing16,
-                                vertical = Spacing.Spacing8
-                            ),
+                        modifier =
+                            Modifier
+                                .fillMaxWidth()
+                                .padding(
+                                    horizontal = Spacing.Spacing16,
+                                    vertical = Spacing.Spacing8,
+                                ),
                         verticalAlignment = Alignment.CenterVertically,
-                    )
-                    {
+                    ) {
                         Box(
                             modifier = Modifier.weight(1f),
-                        )
-                        {
+                        ) {
                             Markdown(
-                                content=title,
+                                content = title,
                                 imageTransformer = Coil3ImageTransformerImpl,
                                 typography = mdTypographyKey,
                                 dimens = markdownDimensions,
                                 padding = padding,
                                 components = components,
-                                modifier = Modifier.padding(0.dp)
+                                modifier = Modifier.padding(0.dp),
                             )
                         }
                         content?.let {
                             Spacer(Modifier.width(8.dp))
                             Box(
-                                modifier = Modifier
-                                    .widthIn(max=halfWidth)
-                                    .wrapContentWidth(Alignment.End),
-                            )
-                            {
+                                modifier =
+                                    Modifier
+                                        .widthIn(max = halfWidth)
+                                        .wrapContentWidth(Alignment.End),
+                            ) {
                                 Markdown(
                                     content,
                                     imageTransformer = Coil3ImageTransformerImpl,
@@ -191,7 +206,7 @@ fun Indicator(
                                     dimens = markdownDimensions,
                                     padding = padding,
                                     components = components,
-                                    modifier = Modifier.padding(0.dp)
+                                    modifier = Modifier.padding(0.dp),
                                 )
                             }
                         }
