6fcee51a2edbd5c3c8493466727a3ef1900b8c98
Manu Muñoz <manu.munoz@dhis2.org>
2026-02-19 10:20:03 +0100

    feat: [ANDROAPP-7475] add fullscreen dialog (#524)

---
+++ designsystem/src/commonMain/kotlin/org/hisp/dhis/mobile/ui/designsystem/component/FullScreenDialog.kt
@@ -0,0 +1,71 @@
+package org.hisp.dhis.mobile.ui.designsystem.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import org.hisp.dhis.mobile.ui.designsystem.theme.SurfaceColor
+
+/**
+ * DHIS2 Full Screen Dialog.
+ * @param modifier: allows a modifier to be passed externally.
+ * @param onDismiss: is a callback to dismiss the full screen dialog component.
+ */
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FullScreenDialog(
+    modifier: Modifier = Modifier,
+    onDismiss: () -> Unit,
+    content: @Composable ColumnScope.() -> Unit,
+) {
+    Dialog(
+        properties =
+            DialogProperties(
+                usePlatformDefaultWidth = false,
+                dismissOnClickOutside = false,
+            ),
+        onDismissRequest = onDismiss,
+    ) {
+        Scaffold(
+            modifier = modifier,
+            containerColor = SurfaceColor.SurfaceBright,
+            topBar = {
+                TopBar(
+                    colors =
+                        TopAppBarDefaults.topAppBarColors(
+                            containerColor = SurfaceColor.SurfaceBright,
+                        ),
+                    navigationIcon = {
+                        IconButton(
+                            onClick = onDismiss,
+                            modifier = Modifier.testTag("FULL_SCREEN_DIALOG_BACK_BUTTON"),
+                            icon = {
+                                Icon(
+                                    imageVector = Icons.AutoMirrored.Outlined.ArrowBack,
+                                    contentDescription = "Back Button",
+                                )
+                            },
+                        )
+                    },
+                    title = {},
+                    actions = {},
+                )
+            },
+            content = {
+                Column(Modifier.padding(it)) {
+                    content()
+                }
+            },
+        )
+    }
+}
