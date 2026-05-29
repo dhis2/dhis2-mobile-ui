b626c546ace759af53fd0331963a09ae02e2ac3a
FerdyRod <ferdy@dhis2.org>
2026-05-29 14:57:10 +0200

    updates commit script to tracked new generated images

--- .github/workflows/scripts/commit-changes.sh
+++ .github/workflows/scripts/commit-changes.sh
@@ -3,9 +3,8 @@ readonly GIT_HAS_CHANGES=1 # 0 = clean, 1 = dirty
 git config user.name "GitHub Actions Bot"
 git config user.email "<android@dhis2.org>"
 
-# Following git command checks the git diff of staged files
-# and assigns the exit code to GIT_DIFF_STATUS variable.
-git diff --quiet; GIT_DIFF_STATUS=$?
+# Check for any working tree changes (modified tracked files OR new untracked files)
+git status --porcelain | grep -q .; GIT_DIFF_STATUS=$?
 
 # If the GIT_DIFF_STATUS is 1 (GIT_HAS_CHANGES) then we know
 # there are changes and we can commit the changes and push
